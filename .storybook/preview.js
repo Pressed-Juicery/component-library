dfd9498c04dbde65bd47eff71b7c065fd002873d
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-04-13 14:45:32 -0700

    feat(addons): install viewport addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
-import { addDecorator } from '@storybook/vue';
+import { addDecorator, addParameters } from '@storybook/vue';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { withA11y } from '@storybook/addon-a11y';
 
 addDecorator(withA11y);
+
+addParameters({
+	viewport: {
+		viewports: INITIAL_VIEWPORTS,
+		defaultViewport: 'responsive',
+	},
+});
