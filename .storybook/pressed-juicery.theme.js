5a09301981c700b318fe6967edad9fbba9fb1708
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-04-14 07:26:45 -0700

    feat(theme): update the theme to better match the brand

---
+++ .storybook/pressed-juicery.theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export const theme = create({
+	base: 'light',
+	appBg: '#F6F4EC',
+	colorSecondary: '#282321',
+	brandImage: 'logo.svg',
+	brandTitle: 'Pressed Juicery',
+	brandUrl: 'https://pressedjuicery.com',
+});
