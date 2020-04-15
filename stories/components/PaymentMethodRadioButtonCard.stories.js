28b2952646646ae4413c7cd66cc9dd9bcb557ffc
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-15 15:08:37 -0700

    feat(payment radio button card stories): add payment method radio card stories

---
+++ stories/components/PaymentMethodRadioButtonCard.stories.js
@@ -0,0 +1,174 @@
+import PaymentMethodRadioButtonCard from '../../components/PaymentMethodRadioButtonCard';
+
+export default {
+	title: 'PaymentMethodRadioButtonCard',
+};
+
+const mastercard = {
+	uniqueNumberIdentifier: 'qwerty12345',
+	maskedNumber: '123456789',
+	cardType: 'mastercard',
+	expirationDate: '11/22',
+	billingAddress: {
+		postalCode: '93711',
+	},
+};
+
+const visa = {
+	uniqueNumberIdentifier: 'asdfg7890',
+	maskedNumber: '987654321',
+	cardType: 'visa',
+	expirationDate: '10/22',
+	billingAddress: {
+		postalCode: '93799',
+	},
+};
+
+const discover = {
+	uniqueNumberIdentifier: 'zxcv9876',
+	maskedNumber: '11111111111',
+	cardType: 'discover',
+	expirationDate: '02/22',
+	billingAddress: {
+		postalCode: '93709',
+	},
+};
+
+const americanExpress = {
+	uniqueNumberIdentifier: 'poiu12345',
+	maskedNumber: '2222222222',
+	cardType: 'american express',
+	expirationDate: '05/22',
+	billingAddress: {
+		postalCode: '93776',
+	},
+};
+
+const unknown = {
+	uniqueNumberIdentifier: 'lkjhg12345',
+	maskedNumber: '45654343563',
+	cardType: 'Excelsior',
+	expirationDate: '09/22',
+	billingAddress: {
+		postalCode: '93678',
+	},
+};
+
+const cardTemplate = `
+	<payment-method-radio-button-card class="radio-button-card"
+	                                  :paymentMethod="cardData.paymentMethod"
+	                                  v-model="cardData.selectedPaymentMethod">
+	</payment-method-radio-button-card>
+`;
+
+export function withMastercard() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: mastercard,
+					selectedPaymentMethod: visa,
+				}),
+			},
+		},
+	};
+}
+
+export function withVisa() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: visa,
+					selectedPaymentMethod: discover,
+				}),
+			},
+		},
+	};
+}
+
+export function withDiscover() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: discover,
+					selectedPaymentMethod: visa,
+				}),
+			},
+		},
+	};
+}
+
+export function withAmericanExpress() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: americanExpress,
+					selectedPaymentMethod: visa,
+				}),
+			},
+		},
+	};
+}
+
+export function withUnknownPaymentMethod() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: unknown,
+					selectedPaymentMethod: visa,
+				}),
+			},
+		},
+	};
+}
+
+export function Unchecked() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: mastercard,
+					selectedPaymentMethod: visa,
+				}),
+			},
+		},
+	};
+}
+
+export function Checked() {
+	return {
+		components: { PaymentMethodRadioButtonCard },
+		template: cardTemplate,
+
+		props: {
+			cardData: {
+				default: () => ({
+					paymentMethod: mastercard,
+					selectedPaymentMethod: mastercard,
+				}),
+			},
+		},
+	};
+}
