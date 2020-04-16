430908027d863331f9979ccb2a8e75d5b00647f0
Spencer <slalexander92@gmail.com>
2020-04-16 15:21:23 -0700

    style(imagebanner.stories): lint imagebanner stories

--- stories/ImageBanner/ImageBanner.stories.js
+++ stories/ImageBanner/ImageBanner.stories.js
@@ -1,31 +1,32 @@
-import Vue from 'vue';
-import { action } from '@storybook/addon-actions';
+import '../../styles/buttons.scss';
 import ImageBanner from './ImageBanner.component';
 
-import '../../styles/buttons.scss';
-
 export default {
-  component: ImageBanner,
-  title: 'ImageBanner',
+	component: ImageBanner,
+	title: 'ImageBanner',
 };
 
-export const Banner1 = () => ({
-  components: { ImageBanner },
-  template: `
-    <ImageBanner :imageDesktop="imageDesktop" :imageMobile="imageMobile">
-      <template v-slot:title>
-        <h1>This is the title</h1>
-      </template>
-      <template v-slot:subtitle>
-        <h3>This is the subtitle</h3>
-      </template>
-      <template v-slot:button>
-        <button>Shop Now</button>
-      </template>
-    </ImageBanner>
-  `,
-  data: () => ({
-    imageDesktop: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/bragg-landing/hero-image.jpg',
-    imageMobile: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/bragg-landing/hero-image-mobile.jpg',
-  })
-});
+export function Banner1() {
+	return {
+		components: { ImageBanner },
+		template: `
+			<ImageBanner :imageDesktop="imageDesktop" :imageMobile="imageMobile">
+				<template v-slot:title>
+					<h1>This is the title</h1>
+				</template>
+				<template v-slot:subtitle>
+					<h3>This is the subtitle</h3>
+				</template>
+				<template v-slot:button>
+					<button>Shop Now</button>
+				</template>
+			</ImageBanner>
+		`,
+		data() {
+			return {
+				imageDesktop: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/bragg-landing/hero-image.jpg',
+				imageMobile: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/bragg-landing/hero-image-mobile.jpg',
+			};
+		},
+	};
+}
