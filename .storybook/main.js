d2fa8e8686b45201920ef212ae28207be712cb6f
Spencer <slalexander92@gmail.com>
2020-04-21 14:50:55 -0700

    chore(main): add path resolution for '~' and add rules to use svg-inline-loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,28 @@
+const path = require('path');
+
 module.exports = {
+	webpackFinal: config => {
+
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'~': path.resolve(__dirname, '../'),
+		};
+
+		config.module.rules
+			.filter(moduleRules => moduleRules.test.toString().includes('svg'))
+			.forEach((rule) => { rule.test = /\.(png|jpe?g|gif)$/ })
+
+    config.module.rules = [
+			...config.module.rules,
+			{
+				test: /\.svg$/,
+				loader: 'svg-inline-loader',
+				exclude: /node_modules/,
+			},
+		];
+
+		return config;
+	},
 	stories: ['../stories/**/*.stories.js'],
 	addons: [
 		'@storybook/addon-actions',
