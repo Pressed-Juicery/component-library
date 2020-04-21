63f5041f8003688ff9e9c0288686e47e2e9c938d
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-21 12:36:08 -0700

    feat(radio button card): use card and radio button components

--- components/radio-button-card.vue
+++ components/radio-button-card.vue
@@ -1,18 +1,22 @@
 <template>
 	<label class="radio-button-card">
-		<span class="radio-button-card__left-content"><slot name="main-content" /></span>
-		<span class="radio-button-card__top-right-content"><slot name="top-right-content" /></span>
-
-		<input class="radio-button-card__input"
-		       type="radio"
-		       :value="item"
-		       v-model="radioButtonValue" />
-		<span class="radio-button-card__radio-button" />
+		<card>
+			<span class="radio-button-card__content">
+				<span class="radio-button-card__left-content"><slot name="main-content" /></span>
+				<span class="radio-button-card__top-right-content"><slot name="top-right-content" /></span>
+				<radio-button class="radio-button" :item="item" v-model="selectedItem" />
+			</span>
+		</card>
 	</label>
 </template>
 
 <script>
+import Card from './card';
+import RadioButton from './radio-button';
+
 export default {
+	components: { Card, RadioButton },
+
 	props: {
 		value: {
 			type: Object,
@@ -25,65 +29,35 @@ export default {
 	},
 
 	computed: {
-		radioButtonValue: {
-			get: function () { return this.value },
-			set: function (value) { this.$emit('input', value) },
+		selectedItem: {
+			get() {
+				return this.value;
+			},
+			set() { this.$emit('input', this.item) },
 		},
 	},
 };
 </script>
 
 <style scoped lang="scss">
-@import '../styles/mixins';
 @import '../styles/variables';
 
-$card-padding: $spacing-05;
-$card-padding-mobile: $spacing-03;
-
 .radio-button-card {
-	@include card;
-
-	position: relative;
-	display: flex;
-	justify-content: space-between;
+	display: block;
 	margin-bottom: $spacing-06;
-	padding: $card-padding;
-	cursor: pointer;
+}
+
+.radio-button-card__content {
+	display: flex;
+	position: relative;
 }
 
 .radio-button-card__left-content {
-	flex: 6 2;
-	margin-right: $spacing-05;
+	flex: 1;
+	margin-right: $spacing-06;
 }
 
 .radio-button-card__top-right-content {
 	margin-bottom: $spacing-08;
-}
-
-.radio-button-card__input {
-	display: none;
-}
-
-.radio-button-card__radio-button {
-	@include card-radio-button;
-
-	position: absolute;
-	top: 50%;
-	transform: translateY(-50%);
-	right: $card-padding;
-}
-
-.radio-button-card__input:checked + .radio-button-card__radio-button {
-	background-color: $gray-50;
-}
-
-@media (max-width: 400px) {
-	.radio-button-card {
-		padding: $card-padding-mobile;
-	}
-
-	.radio-button-card__radio-button {
-		right: $card-padding-mobile;
-	}
 }
 </style>
