e709e6de5acaa3335ed8735a433c915dd8760fd9
Spencer <slalexander92@gmail.com>
2020-04-21 14:51:55 -0700

    feat(svg-icon): add svg icon component

---
+++ stories/svg-icon/svg-icon.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="svg-icon" v-html="svg"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: () => '',
+    }
+  },
+  computed: {
+    svg() {
+      return require(`~/assets/${this.name}.svg`);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .svg-icon{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
