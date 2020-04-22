5dcc21296b81f055b8cb62914fcb7e6889ed8eb5
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-04-22 16:41:35 -0700

    refactor(accordion): extract arrow icon into its own component

--- components/accordion.vue
+++ components/accordion.vue
@@ -3,14 +3,7 @@
 		<div class="accordion__question-container" @click="toggleAnswer">
 			<h3 class="accordion__question">{{ accordionContent.question }}</h3>
 			<div :class="['accordion__button', {'accordion__button--toggle': showAnswer}]">
-				<svg class="accordion__button-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-					preserveAspectRatio="xMidYMid" width="31.844" height="17.91" viewBox="0 0 31.844 17.91">
-					<path d="M15.922,17.918 C15.647,17.918 15.385,17.804 15.197,17.603 L0.291,1.689 C-0.084,1.288
-						-0.064, 0.659 0.336,0.283 C0.737,-0.092 1.366,-0.073 1.741,0.328 L15.922,15.469 L30.103,0.328
-						C30.478,-0.072 31.107,-0.093 31.508,0.283 C31.908,0.659 31.928,1.288 31.553,1.689 L16.647,17.603
-						C16.459,17.804 16.197, 17.918 15.922,17.918 Z"
-					/>
-				</svg>
+				<arrow-down-icon class="accordion__button-icon" />
 			</div>
 		</div>
 		<div :class="['accordion__answer-container', {'accordion__answer-container--toggle': showAnswer}]">
@@ -22,8 +15,11 @@
 </template>
 
 <script>
+import ArrowDownIcon from './icons/arrow-down-icon';
+
 export default {
 	name: 'accordion',
+	components: { ArrowDownIcon },
 	props: {
 		accordionContent: Object,
 	},
