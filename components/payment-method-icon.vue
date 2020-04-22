af1e426d6204fd41492acf89a7e48548f7c450dc
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-22 12:56:32 -0700

    feat(payment method icon): add payment method icon component

---
+++ components/payment-method-icon.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<american-express-icon v-if="type.toLowerCase() === 'american express'" />
+		<discover-icon v-else-if="type.toLowerCase() === 'discover'" />
+		<master-card-icon v-else-if="type.toLowerCase() === 'mastercard'" />
+		<visa-icon v-else-if="type.toLowerCase() === 'visa'" />
+		<unknown-payment-method-icon v-else />
+	</div>
+</template>
+
+<script>
+import AmericanExpressIcon from './icons/american-express-icon';
+import DiscoverIcon from './icons/discover-icon';
+import MasterCardIcon from './icons/master-card-icon';
+import UnknownPaymentMethodIcon from './icons/unknown-payment-method-icon';
+import VisaIcon from './icons/visa-icon';
+
+export default {
+	components: { AmericanExpressIcon, DiscoverIcon, MasterCardIcon, UnknownPaymentMethodIcon, VisaIcon },
+
+	props: {
+		type: String,
+	},
+}
+</script>
