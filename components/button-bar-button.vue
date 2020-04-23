b0da8f74f1110139a0f0d9cb520f83d0dd4e8b4c
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-23 14:54:49 -0700

    feat(button bar button): add button bar button component

---
+++ components/button-bar-button.vue
@@ -0,0 +1,48 @@
+<template>
+	<button class="button-bar-button"
+	        :class="{ 'button-bar-button--active': isSelected }"
+	        @click="$emit('select')" >
+		<slot />
+	</button>
+</template>
+
+<script>
+
+export default {
+	props: {
+		isSelected: Boolean,
+	},
+};
+
+</script>
+
+<style scoped lang="scss">
+	@import '../styles/variables';
+
+	.button-bar-button {
+		border: $border-width solid $border-color;
+		border-right: none;
+		border-radius: 0;
+		background-color: transparent;
+		color: $gray-100;
+	}
+
+	.button-bar-button:focus {
+		outline: none;
+	}
+
+	.button-bar-button:first-child {
+		border-top-left-radius: $border-radius;
+		border-bottom-left-radius: $border-radius;
+	}
+
+	.button-bar-button:last-child {
+		border-right: $border-width solid $border-color;
+		border-top-right-radius: $border-radius;
+		border-bottom-right-radius: $border-radius;
+	}
+
+	.button-bar-button--active {
+		background-color: $beige;
+	}
+</style>
