48470ef258a2596d3a2977cdad760bbf97d135fb
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-23 10:54:42 -0700

    refactor(address radio button card stories): move more data inside functions

--- stories/address-radio-button-card.js
+++ stories/address-radio-button-card.js
@@ -5,52 +5,33 @@ export default {
 	component: AddressRadioButtonCard,
 };
 
-const defaultAddress = {
+const firstAddress = {
 	address1: '1 Meadow Lane',
 	city: 'Portland',
 	province: 'OR',
 	zip: '27705',
-	phone: '5593454567',
 };
 
-const addressWithSecondaryAddress = {
-	address1: '42 Lombard',
-	address2: 'Apt. 42',
-	city: 'San Francisco',
-	province: 'CA',
-	zip: '93723',
-	phone: '5592343234',
-};
-
-const addressWithNoPhone = {
+const secondAddress = {
 	address1: '1 Duke University Drive',
 	city: 'Durham',
 	province: 'NC',
 	zip: '27709',
 };
 
-const addressWithDashedPhone = {
+const thirdAddress = {
 	address1: '1 Amsterdam',
 	city: 'New York',
 	province: 'NY',
 	zip: '27705',
-	phone: '205-234-3456',
 };
 
-const longAddress = {
-	address1: '1 Meadow Lane!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
-	city: 'Portland',
-	province: 'OR',
-	zip: '27705',
-	phone: '5593454567',
-};
-
-export function Default() {
+export function Overview() {
 	return {
 		components: { AddressRadioButtonCard },
-		template: '<address-radio-button-card title="Title" :address="defaultAddress" v-model="selectedAddress" />',
+		template: '<address-radio-button-card title="Title" :address="firstAddress" v-model="selectedAddress" />',
 
-		data() { return { defaultAddress, selectedAddress: addressWithNoPhone } },
+		data() { return { firstAddress, selectedAddress: null } },
 	};
 }
 
@@ -63,16 +44,26 @@ export function SecondaryAddress() {
 			                           v-model="selectedAddress" />
         `,
 
-		data() { return { addressWithSecondaryAddress, selectedAddress: defaultAddress } },
+		data() {
+			return {
+				addressWithSecondaryAddress: { ...firstAddress, address2: 'Apt. 42' },
+				selectedAddress: null,
+			};
+		},
 	};
 }
 
-export function NoPhone() {
+export function Phone() {
 	return {
 		components: { AddressRadioButtonCard },
-		template: '<address-radio-button-card title="Title" :address="addressWithNoPhone" v-model="selectedAddress" />',
+		template: '<address-radio-button-card title="Title" :address="addressWithPhone" v-model="selectedAddress" />',
 
-		data() { return { addressWithNoPhone, selectedAddress: defaultAddress } },
+		data() {
+			return {
+				addressWithPhone: { ...secondAddress, phone: '5593454567' },
+				selectedAddress: null,
+			};
+		},
 	};
 }
 
@@ -85,25 +76,30 @@ export function DashedPhone() {
 			                           v-model="selectedAddress" />
 		`,
 
-		data() { return { addressWithDashedPhone, selectedAddress: defaultAddress } },
+		data() {
+			return {
+				addressWithDashedPhone: { ...thirdAddress, phone: '205-234-3456' },
+				selectedAddress: null,
+			};
+		},
 	};
 }
 
 export function NoTitle() {
 	return {
 		components: { AddressRadioButtonCard },
-		template: '<address-radio-button-card :address="defaultAddress" v-model="selectedAddress" />',
+		template: '<address-radio-button-card :address="firstAddress" v-model="selectedAddress" />',
 
-		data() { return { defaultAddress, selectedAddress: addressWithNoPhone } },
+		data() { return { firstAddress, selectedAddress: null } },
 	};
 }
 
 export function Checked() {
 	return {
 		components: { AddressRadioButtonCard },
-		template: '<address-radio-button-card title="Title" :address="defaultAddress" v-model="selectedAddress" />',
+		template: '<address-radio-button-card title="Title" :address="firstAddress" v-model="selectedAddress" />',
 
-		data() { return { defaultAddress, selectedAddress: defaultAddress } },
+		data() { return { firstAddress, selectedAddress: firstAddress } },
 	};
 }
 
@@ -112,7 +108,12 @@ export function LongAddress() {
 		components: { AddressRadioButtonCard },
 		template: '<address-radio-button-card title="Title" :address="longAddress" v-model="selectedAddress" />',
 
-		data() { return { longAddress, selectedAddress: addressWithNoPhone } },
+		data() {
+			return {
+				longAddress: { ...firstAddress, address1: '1 Meadow Lane!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!' },
+				selectedAddress: null,
+			};
+		},
 	};
 }
 
@@ -131,8 +132,8 @@ export function RadioGroup() {
 
 		data() {
 			return {
-				addresses: [defaultAddress, addressWithSecondaryAddress, addressWithNoPhone, addressWithDashedPhone],
-				selectedAddress: defaultAddress,
+				addresses: [firstAddress, secondAddress, thirdAddress],
+				selectedAddress: firstAddress,
 			};
 		},
 	};
