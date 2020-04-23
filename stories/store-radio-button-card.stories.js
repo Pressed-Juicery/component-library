a9c42f1b32476e9855653fb76efb3938c663db1c
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-23 10:00:41 -0700

    refactor(store radio button card stories): move more of the data into the functions

--- stories/store-radio-button-card.stories.js
+++ stories/store-radio-button-card.stories.js
@@ -4,49 +4,23 @@ export default {
 	title: 'StoreRadioButtonCard',
 };
 
-const storeHours = {
-	sunday: { start: 1100, end: 2000 },
-	monday: { start: 800, end: 2100 },
-	tuesday: { start: 800, end: 2100 },
-	wednesday: { start: 800, end: 2100 },
-	thursday: { start: 800, end: 2100 },
-	friday: { start: 800, end: 2100 },
-	saturday: { start: 1100, end: 2000 },
-};
-
-const variedStoreHours = {
-	sunday: { start: 1100, end: 2000 },
-	monday: { start: 700, end: 2100 },
-	tuesday: { start: 800, end: 2100 },
-	wednesday: { start: 800, end: 2200 },
-	thursday: { start: 800, end: 2100 },
-	friday: { start: 800, end: 2000 },
-	saturday: { start: 700, end: 1900 },
-};
-
-const defaultStore = {
-	id: 123,
+const firstStore = {
 	name: 'Beaverton Town Center',
 	streetAddress: '1 Canyon Road',
 	locality: 'Beaverton',
 	region: 'OR',
 	postal: '27705',
-	storeHours,
 };
 
-const storeWithExtendedAddress = {
-	id: 456,
+const secondStore = {
 	name: 'Pioneer Square',
 	streetAddress: '1 Yamhill Road',
-	extendedAddress: 'Suite 42',
 	locality: 'Portland',
 	region: 'OR',
 	postal: '27705',
-	storeHours,
 };
 
-const storeWithoutHours = {
-	id: 789,
+const thirdStore = {
 	name: 'Pearl District',
 	streetAddress: '42 10th street',
 	locality: 'Portland',
@@ -54,34 +28,14 @@ const storeWithoutHours = {
 	postal: '27705',
 };
 
-const storeWithVariedHours = {
-	id: 567,
-	name: 'Beaverton Farmers Market',
-	streetAddress: '1 Hall',
-	locality: 'Beaverton',
-	region: 'OR',
-	postal: '27705',
-	storeHours: variedStoreHours,
-};
-
-const storeWithoutDistance = {
-	id: 234,
-	name: 'Hawthorne',
-	streetAddress: '1 Hawthorne',
-	locality: 'Portland',
-	region: 'OR',
-	postal: '27705',
-	storeHours,
-};
-
 export function Default() {
 	return {
 		components: { StoreRadioButtonCard },
 		template: `
-			<store-radio-button-card :store="defaultStore" v-model="selectedStore" />
+			<store-radio-button-card :store="firstStore" v-model="selectedStore" />
 		`,
 
-		data() { return { defaultStore, selectedStore: storeWithExtendedAddress } },
+		data() { return { firstStore, selectedStore: null } },
 	};
 }
 
@@ -89,10 +43,10 @@ export function Checked() {
 	return {
 		components: { StoreRadioButtonCard },
 		template: `
-			<store-radio-button-card :store="defaultStore" v-model="selectedStore" />
+			<store-radio-button-card :store="firstStore" v-model="selectedStore" />
 		`,
 
-		data() { return { defaultStore, selectedStore: defaultStore } },
+		data() { return { firstStore, selectedStore: firstStore } },
 	};
 }
 
@@ -103,7 +57,12 @@ export function ExtendedAddress() {
 			<store-radio-button-card :store="storeWithExtendedAddress" v-model="selectedStore" />
 		`,
 
-		data() { return { storeWithExtendedAddress, selectedStore: defaultStore } },
+		data() {
+			return {
+				storeWithExtendedAddress: { ...secondStore, extendedAddress: 'Suite 42' },
+				selectedStore: null
+			};
+		},
 	};
 }
 
@@ -111,10 +70,26 @@ export function Hours() {
 	return {
 		components: { StoreRadioButtonCard },
 		template: `
-			<store-radio-button-card :store="storeWithoutHours" v-model="selectedStore" />
+			<store-radio-button-card :store="storeWithHours" v-model="selectedStore" />
 		`,
 
-		data() { return { storeWithoutHours, selectedStore: storeWithExtendedAddress } },
+		data() {
+			return {
+				storeWithHours: {
+					...thirdStore,
+					storeHours: {
+						sunday: { start: 1100, end: 2000 },
+						monday: { start: 800, end: 2100 },
+						tuesday: { start: 800, end: 2100 },
+						wednesday: { start: 800, end: 2100 },
+						thursday: { start: 800, end: 2100 },
+						friday: { start: 800, end: 2100 },
+						saturday: { start: 1100, end: 2000 },
+					},
+				},
+				selectedStore: null,
+			};
+		},
 	};
 }
 
@@ -125,7 +100,23 @@ export function VariedHours() {
 			<store-radio-button-card :store="storeWithVariedHours" v-model="selectedStore" />
 		`,
 
-		data() { return { storeWithVariedHours, selectedStore: storeWithExtendedAddress } },
+		data() {
+			return {
+				storeWithVariedHours: {
+					...firstStore,
+					storeHours: {
+						sunday: { start: 1100, end: 2000 },
+						monday: { start: 700, end: 2100 },
+						tuesday: { start: 800, end: 2100 },
+						wednesday: { start: 800, end: 2200 },
+						thursday: { start: 800, end: 2100 },
+						friday: { start: 800, end: 2000 },
+						saturday: { start: 700, end: 1900 },
+					},
+				},
+				selectedStore: null,
+			};
+		},
 	};
 }
 
@@ -151,7 +142,7 @@ export function RadioGroup() {
 		template: `
 			<div>
 				<store-radio-button-card v-for="store in stores"
-				                         :key="store.id"
+				                         :key="store.name"
 				                         :store="store"
 				                         v-model="selectedStore" />
 			</div>
@@ -159,8 +150,8 @@ export function RadioGroup() {
 
 		data() {
 			return {
-				stores: [defaultStore, storeWithExtendedAddress, storeWithoutHours, storeWithVariedHours],
-				selectedStore: defaultStore,
+				stores: [firstStore, secondStore, thirdStore],
+				selectedStore: firstStore,
 			};
 		},
 	};
