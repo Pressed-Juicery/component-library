72deeb52339d3e28ba0f9ea24a4dea1304e2e94c
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-24 16:06:11 -0700

    refactor(button bar button stories): update for updated button bar button component

--- stories/button-bar-button.js
+++ stories/button-bar-button.js
@@ -21,7 +21,9 @@ export function Selected() {
 	return {
 		components: { ButtonBarButton },
 		template: `
-			<button-bar-button :isSelected="true">
+			<button-bar-button style="margin:20px; padding:20px"
+			                   value="Button Bar Button"
+			                   selectedButton="Button Bar Button">
 				<div>Button Bar Button</div>
 				<div>Customizable Content</div>
 			</button-bar-button>
@@ -33,18 +35,61 @@ export function ButtonBar() {
 	return {
 		components: { ButtonBarButton },
 		template: `
-			<div>
-				<button-bar-button v-for="content in contents"
-				                   style="padding:20px"
-				                   :isSelected="selectedButton === content"
-				                   @select-button="selectedButton = content">
-					{{ content }}
+			<div style="margin:20px; padding:20px; display:flex">
+				<button-bar-button style="padding:20px"
+				                   v-for="(content, index) in contents"
+				                   :key="index"
+				                   :value="content"
+				                   :selectedButton="selectedItem"
+				                   @select-button="value => selectedItem = value">
+					<div>{{ content.title }}</div>
+					<div>{{ content.detail }}</div>
 				</button-bar-button>
 			</div>
 		`,
 
 		data() {
-			return { contents: ['First', 'Second', 'Last'], selectedButton: null };
+			return {
+				contents: [
+					{ title: 'This Button', detail: 'Bar Is' },
+					{ title: 'Fully', detail: 'Customizable' },
+					{ title: 'Its Contents', detail: 'Are Dynamic' },
+				],
+				selectedItem: null,
+			};
 		},
+	};
+}
+
+export function VariedButtonBar() {
+	return {
+		components: { ButtonBarButton },
+		template: `
+			<div style="margin:20px; padding:20px; display:flex">
+				<button-bar-button style="padding:20px"
+				                   value="A"
+				                   :selectedButton="selectedButton"
+				                   @select-button="value => selectedButton = value">
+					<div>Basic Content</div>
+				</button-bar-button>
+				<button-bar-button style="padding:20px"
+				                   value="B"
+				                   :selectedButton="selectedButton"
+				                   @select-button="value => selectedButton = value">
+					<div>This Button</div>
+					<div>Has Two Lines</div>
+				</button-bar-button>
+				<button-bar-button style="padding:20px"
+				                   value="C"
+				                   :selectedButton="selectedButton"
+				                   @select-button="value => selectedButton = value">
+					<div>Line <span style="font-weight:bold">One</span></div>
+					<div>Line Two</div>
+					<div>Line Three</div>
+				</button-bar-button>
+			</div>
+		`,
+
+		data() { return { selectedButton: 'A' } },
 	};
 }
