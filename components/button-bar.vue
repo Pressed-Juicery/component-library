2c25539b71deee8f61a78e60d6a36c8e8ce9ab79
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-24 16:04:11 -0700

    refactor(button bar): remove unneeded code

--- components/button-bar.vue
+++ components/button-bar.vue
@@ -1,38 +1,8 @@
 <template>
 	<div class="button-bar__bar">
-		<button-bar-button v-for="(content, index) in contents"
-		                   :key="index"
-		                   :isSelected="selectedButton === content"
-		                   @select-button="selectedButton = content">
-			<slot :content="content"/>
-		</button-bar-button>
+		<slot />
 	</div>
 </template>
-
-<script>
-import ButtonBarButton from './button-bar-button';
-
-export default {
-	components: { ButtonBarButton },
-
-	props: {
-		contents: {
-			type: Array,
-			required: true,
-		},
-		selectedItem: {
-			default: null,
-		},
-	},
-
-	computed: {
-		selectedButton: {
-			get() { return this.selectedItem },
-			set(value) { this.$emit('select-item', value) },
-		},
-	},
-};
-</script>
 
 <style scoped lang="scss">
 	.button-bar__bar {
