1fe9ba2f4295b31b315892c465d990526ad15283
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-24 09:14:46 -0700

    feat(button bar stories): add longer content story

--- stories/button-bar.stories.js
+++ stories/button-bar.stories.js
@@ -77,6 +77,34 @@ export function VariedContent() {
 	};
 }
 
+export function LongerContent() {
+	return {
+		components: { ButtonBar },
+		template: `
+			<div>
+				<button-bar style="margin:40px"
+				            :contents="contents"
+				            :selected="selectedButton"
+				            @select="value => selectedButton = value">
+					<template slot-scope="{ content }">
+						<div style="padding:20px">
+							<div v-if="content.primary" style="font-weight:bold">{{ content.primary }}</div>
+							<div v-if="content.secondary">{{ content.secondary }}</div>
+							<div v-if="content.tertiary">{{ content.tertiary }}</div>
+						</div>
+					</template>
+				</button-bar>
+			</div>
+		`,
+
+		data() {
+			return {
+				contents: [
+					{ primary: 'Primary', secondary: 'Secondary' },
+					{ primary: 'This is a longer button title.' },
+					{ primary: 'Primary', secondary: 'Secondary' },
+					{ secondary: 'This is excessively longer button content.' },
+				],
 				selectedButton: null,
 			};
 		},
