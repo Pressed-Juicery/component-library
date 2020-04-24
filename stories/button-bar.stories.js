e3237c032011817f99168a7ec14eee2317d42523
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-24 09:14:12 -0700

    feat(button bar stories): add varied content story

--- stories/button-bar.stories.js
+++ stories/button-bar.stories.js
@@ -41,6 +41,42 @@ export function Overview() {
 		},
 	};
 }
+
+export function VariedContent() {
+	return {
+		components: { ButtonBar },
+		template: `
+			<div>
+				<button-bar style="margin:40px"
+				            :contents="contents"
+				            :selected="selectedButton"
+				            @select="value => selectedButton = value">
+					<template slot-scope="{ content }">
+						<div style="padding:20px">
+							<div v-if="content.primary" style="font-weight:bold">{{ content.primary }}</div>
+							<div v-if="content.secondary">{{ content.secondary }}</div>
+							<div v-if="content.tertiary">{{ content.tertiary }}</div>
+						</div>
+					</template>
+				</button-bar>
+			</div>
+		`,
+
+		data() {
+			return {
+				contents: [
+					{ primary: 'Primary' },
+					{ primary: 'Primary', secondary: 'Secondary' },
+					{ primary: 'Primary', secondary: 'Secondary', tertiary: 'Tertiary' },
+					{ primary: 'Primary', tertiary: 'Tertiary' },
+					{ secondary: 'Secondary', tertiary: 'Tertiary' },
+				],
+				selectedButton: null,
+			};
+		},
+	};
+}
+
 				selectedButton: null,
 			};
 		},
