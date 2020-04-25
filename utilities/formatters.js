0857cf9642e879c2ebce2339e3128dd2b3f4d5d0
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-04-24 22:20:47 -0700

    feat(formatters): only format phone numbers with 10 and only 10 digits

--- utilities/formatters.js
+++ utilities/formatters.js
@@ -1,3 +1,3 @@
 export function formatPhoneNumber(phone) {
-	return (phone || '').replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+	return (phone || '').replace(/^(\d{3})(\d{3})(\d{4})$/, '$1-$2-$3');
 }
