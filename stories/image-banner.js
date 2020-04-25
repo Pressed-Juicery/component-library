1fd65e1bb1b4d24f20ed5228d72d168aedfdcf34
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-04-25 08:44:04 -0700

    refactor(image-banner story): use kebab casing for props

--- stories/image-banner.js
+++ stories/image-banner.js
@@ -10,7 +10,7 @@ export function Banner1() {
 	return {
 		components: { ImageBanner },
 		template: `
-			<ImageBanner :srcDesktop="srcDesktop" :srcMobile="srcMobile" alt="hello">
+			<ImageBanner :src-desktop="srcDesktop" :src-mobile="srcMobile" alt="hello">
 				<div>
 					<h1>Hello World</h1>
 					<h2>This is some text</h2>
