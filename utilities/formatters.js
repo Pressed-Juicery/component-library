590c43f3b60c163a4f6f7160e296831da78045f7
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-27 11:56:31 -0700

    feat(formatters): add currency function

--- utilities/formatters.js
+++ utilities/formatters.js
@@ -1,3 +1,12 @@
+export function currency(amount) {
+	if (window.isNaN(amount)) return '';
+
+	const prefix = amount < 0 ? '-$' : '$';
+	const fixedPointAmount = Math.abs(amount).toFixed(2);
+
+	return prefix + fixedPointAmount;
+}
+
 export function formatPhoneNumber(phone) {
 	return (phone || '').replace(/^(\d{3})(\d{3})(\d{4})$/, '$1-$2-$3');
 }
