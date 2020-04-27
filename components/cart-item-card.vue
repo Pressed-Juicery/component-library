f77192dc9bae78df266c007e15ffe60fe141b461
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-27 16:16:02 -0700

    feat(cart item card): add cart item card component

---
+++ components/cart-item-card.vue
@@ -0,0 +1,73 @@
+<template>
+	<card class="cart-item-card">
+		<span class="cart-item-card__image-container">
+			<img class="cart-item-card__image" :src="item.imageUrl" :alt="item.name" />
+		</span>
+
+		<div class="cart-item-card__main-content">
+			<div class="cart-item-card__title">{{ item.name }}</div>
+			<div v-if="item.additionalInformation" class="cart-item-card__additional-info">
+				{{ item.additionalInformation }}
+			</div>
+			<div class="cart-item-card__quantity">Qty {{ item.quantity }}</div>
+		</div>
+
+		<span class="cart-item-card__price">{{ formatPrice(item.discountedTotalPrice) }}</span>
+	</card>
+</template>
+
+<script>
+import Card from './card.vue';
+
+import { currency } from '../utilities/formatters';
+
+export default {
+	components: { Card },
+
+	props: {
+		item: Object,
+	},
+
+	methods: {
+		formatPrice(amount) {
+			return currency(amount);
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+	@import '../styles/variables';
+
+	.cart-item-card {
+		display: flex;
+	}
+
+	.cart-item-card__image-container {
+		width: 80px;
+		margin: 0 auto -12px;
+	}
+
+	.cart-item-card__image {
+		width: 100%;
+	}
+
+	.cart-item-card__main-content {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		margin-left: $spacing-04;
+		margin-right: $spacing-06;
+		word-break: break-word;
+	}
+
+	.cart-item-card__title {
+		margin-bottom: $spacing-02;
+	}
+
+	.cart-item-card__additional-info {
+		margin-bottom: $spacing-02;
+		color: $gray-70;
+	}
+</style>
