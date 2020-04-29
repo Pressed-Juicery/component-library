db1f85a645eab8e454a4994e60f75f8e352a62a0
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-29 08:36:48 -0700

    feat(validated payment method): add validated payment method stories

---
+++ stories/validated-payment-method.js
@@ -0,0 +1,24 @@
+import ValidatedForm from '../components/validated-form';
+import ValidatedPaymentMethod from '../components/validated-payment-method';
+
+export default {
+	title: 'ValidatedPaymentMethod',
+	components: 'ValidatedPaymentMethod',
+};
+
+export function Overview() {
+	return {
+		components: { ValidatedForm, ValidatedPaymentMethod },
+		template: `
+			<validated-form style="margin:20px">
+				<validated-payment-method :authorization="authorizationToken" @change="nonce => test = nonce" />
+				
+				<button style="margin-top:20px; width:120px" type="submit">SUBMIT</button>
+				
+				<p>Test: {{ test }}</p>
+			</validated-form>
+		`,
+
+		data() { return { authorizationToken: 'sandbox_x6kjt36z_3cr8bsrh82p5jrcp', test: null } },
+	};
+}
