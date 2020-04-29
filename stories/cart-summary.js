e705db949df71542ccdc5434d68ed669592e3255
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-29 14:01:29 -0700

    feat(cart summary): add Discounts story

--- stories/cart-summary.js
+++ stories/cart-summary.js
@@ -99,3 +99,45 @@ export function ShippingDeliveryPrice() {
 	};
 }
 
+// eslint-disable-next-line max-lines-per-function
+export function Discounts() {
+	return {
+		components: { CartSummary },
+		template: '<cart-summary :cart="cart" />',
+
+		// eslint-disable-next-line max-lines-per-function
+		data() {
+			return {
+				cart: {
+					itemCount: 101,
+					subtotal: 100,
+					total: 150.5,
+					discounts: [
+						{ name: 'VIP Member Savings', amount: 2.5 },
+						{ name: 'Points Redemption', amount: 6.5 },
+					],
+					items: [
+						{
+							name: 'Strawberry Basil Lemonade',
+							quantity: 10,
+							// eslint-disable-next-line max-len
+							imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
+							discountedTotalPrice: 10,
+						},
+						{
+							name: 'Strawberry Freeze',
+							quantity: 101,
+							// eslint-disable-next-line max-len
+							imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
+							discountedTotalPrice: 122.5,
+							modifiers: {
+								toppings: 'Chocolate Chips, Granola, Coconut',
+							},
+						},
+					],
+				},
+			};
+		},
+	};
+}
+
