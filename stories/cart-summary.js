7a47b29024e9d9f812fe94d665b00e850f6ec2fc
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-29 14:01:01 -0700

    feat(cart summary stories): add shipping delivery price story

--- stories/cart-summary.js
+++ stories/cart-summary.js
@@ -60,3 +60,42 @@ export function Overview() {
 	};
 }
 
+// eslint-disable-next-line max-lines-per-function
+export function ShippingDeliveryPrice() {
+	return {
+		components: { CartSummary },
+		template: '<cart-summary :cart="cart" />',
+
+		// eslint-disable-next-line max-lines-per-function
+		data() {
+			return {
+				cart: {
+					itemCount: 101,
+					subtotal: 100,
+					total: 150.5,
+					shippingPrice: 40,
+					items: [
+						{
+							name: 'Strawberry Basil Lemonade',
+							quantity: 10,
+							// eslint-disable-next-line max-len
+							imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
+							discountedTotalPrice: 10,
+						},
+						{
+							name: 'Strawberry Freeze',
+							quantity: 101,
+							// eslint-disable-next-line max-len
+							imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
+							discountedTotalPrice: 122.5,
+							modifiers: {
+								toppings: 'Chocolate Chips, Granola, Coconut',
+							},
+						},
+					],
+				},
+			};
+		},
+	};
+}
+
