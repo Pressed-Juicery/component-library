0f71da5107e6603e8b143b1e3e15155ef22e8967
Silas Langley <silas.langley@pressedjuicery.com>
2020-04-30 16:01:20 -0700

    feat(address form): add address form component

---
+++ components/address-form.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<validated-input v-model="address.name" />
+	</div>
+</template>
+
+<script>
+import ValidatedInput from './validated-input';
+
+export default {
+	components: { ValidatedInput },
+
+	props: {
+		address: Object,
+	},
+
+	methods: {
+		onChange() {
+			this.$emit('input', this.address);
+		},
+	},
+};
+</script>
