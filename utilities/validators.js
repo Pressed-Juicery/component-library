a7c762855920de1ddb218d81b3db2a9f10086774
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-04 12:56:56 -0700

    feat(validators): add validators

---
+++ utilities/validators.js
@@ -0,0 +1,19 @@
+export function hasMinimumLength(value, { length }) {
+	return isEmpty(value) || value.length >= length;
+}
+
+export function isEmail(value) {
+	return isEmpty(value) || /^\S+@\S+\.\S+$/.test(value);
+}
+
+export function isNotEmpty(value) {
+	return !isEmpty(value);
+}
+
+export function isEmpty(value) {
+	return !value && value !== 0;
+}
+
+export function isValidPhoneNumber(value) {
+	return isEmpty(value) || /^\D*(\d\D*){10}$/.test(value);
+}
