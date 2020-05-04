8cb791b9cbde562798ebb65243fb3f036c8ae807
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-04 09:38:00 -0700

    feat(validated form): add dynamic custom error message story

--- stories/validated-form.js
+++ stories/validated-form.js
@@ -96,3 +96,43 @@ export function DynamicErrorMessage() {
 		data() { return { showError: false } },
 	};
 }
+
+// eslint-disable-next-line max-lines-per-function
+export function DynamicCustomErrorMessage() {
+	return {
+		components: {
+			ValidatedForm,
+			FirstNameInput: {
+				template: '<label>First Name<input v-model="firstName" /></label>',
+
+				data() { return { firstName: null } },
+
+				methods: {
+					isValid() {
+						return Boolean(this.firstName);
+					},
+				},
+			},
+			SecondNameInput: {
+				template: '<label>Second Name<input v-model="secondName" /></label>',
+
+				data() { return { secondName: null } },
+
+				methods: {
+					isValid() {
+						return Boolean(this.secondName);
+					},
+				},
+			},
+		},
+		template: `
+			<validated-form :showError="showError" errorMessage="Form submission failed due to a server error.">
+				<first-name-input />
+				<second-name-input />
+				<button style="display:block; margin-top:20px; width:120px" type="submit">Submit</button>
+			</validated-form>
+		`,
+
+		data() { return { showError: false } },
+	};
+}
