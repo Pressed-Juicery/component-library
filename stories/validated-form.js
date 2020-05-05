51ddf3354115a8713d646564ec8cc92cc7cb8be7
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-05 11:12:58 -0700

    feat(validated form stories): add validated input stories

--- stories/validated-form.js
+++ stories/validated-form.js
@@ -1,7 +1,11 @@
+import { hasMinimumLength, isNotEmpty } from '../utilities/validators';
+
 import ValidatedForm from '../components/validated-form';
+import ValidatedInput from '../components/validated-input';
 
 export default {
 	title: 'ValidatedForm',
+	component: ValidatedForm,
 };
 
 export function Overview() {
@@ -57,82 +61,112 @@ export function CustomErrorMessage() {
 	};
 }
 
-// eslint-disable-next-line max-lines-per-function
-export function DynamicErrorMessage() {
+export function SingleValidatedInput() {
 	return {
-		components: {
-			ValidatedForm,
-			FirstNameInput: {
-				template: '<label>First Name<input v-model="firstName" /></label>',
-
-				data() { return { firstName: null } },
-
-				methods: {
-					isValid() {
-						return Boolean(this.firstName);
-					},
-				},
-			},
-			SecondNameInput: {
-				template: '<label>Second Name<input v-model="secondName" /></label>',
-
-				data() { return { secondName: null } },
-
-				methods: {
-					isValid() {
-						return Boolean(this.secondName);
-					},
-				},
-			},
-		},
+		components: { ValidatedForm, ValidatedInput },
 		template: `
-			<validated-form :showError="showError">
-				<first-name-input />
-				<second-name-input />
-				<button style="display:block; margin-top:20px; width:120px" type="submit">Submit</button>
+			<validated-form>
+				<validated-input type="text"
+				                 label="Label"
+				                 :rules="rules"
+				                 v-model="model" />
+				<button style="margin-top:20px" type="submit">SUBMIT</button>
 			</validated-form>
 		`,
 
-		data() { return { showError: false } },
+		data() {
+			return {
+				model: null,
+				rules: [{
+					validator: isNotEmpty,
+					message: 'Please enter something.',
+				}, {
+					validator: hasMinimumLength,
+					message: 'Must be at least 6 characters long.',
+					options: { length: 6 },
+				}],
+			};
+		},
 	};
 }
 
 // eslint-disable-next-line max-lines-per-function
-export function DynamicCustomErrorMessage() {
+export function MultipleValidatedInputs() {
 	return {
-		components: {
-			ValidatedForm,
-			FirstNameInput: {
-				template: '<label>First Name<input v-model="firstName" /></label>',
-
-				data() { return { firstName: null } },
-
-				methods: {
-					isValid() {
-						return Boolean(this.firstName);
-					},
-				},
-			},
-			SecondNameInput: {
-				template: '<label>Second Name<input v-model="secondName" /></label>',
-
-				data() { return { secondName: null } },
-
-				methods: {
-					isValid() {
-						return Boolean(this.secondName);
-					},
-				},
-			},
-		},
+		components: { ValidatedForm, ValidatedInput },
 		template: `
-			<validated-form :showError="showError" errorMessage="Form submission failed due to a server error.">
-				<first-name-input />
-				<second-name-input />
-				<button style="display:block; margin-top:20px; width:120px" type="submit">Submit</button>
+			<validated-form>
+				<validated-input type="text"
+				                 label="First Name"
+				                 :rules="firstNameRules"
+				                 v-model="firstName" />
+
+				<validated-input type="text"
+				                 label="Password"
+				                 :rules="passwordRules"
+				                 v-model="password" />
+				<button style="margin-top:20px" type="submit">SUBMIT</button>
 			</validated-form>
 		`,
 
-		data() { return { showError: false } },
+		data() {
+			return {
+				firstName: null,
+				password: null,
+				firstNameRules: [{
+					validator: isNotEmpty,
+					message: 'Please enter something.',
+				}],
+				passwordRules: [{
+					validator: isNotEmpty,
+					message: 'Please enter something.',
+				}, {
+					validator: hasMinimumLength,
+					message: 'Must be at least 6 characters long.',
+					options: { length: 6 },
+				}],
+			};
+		},
+	};
+}
+
+// eslint-disable-next-line max-lines-per-function
+export function CustomValidatedInputErrorMessage() {
+	return {
+		components: { ValidatedForm, ValidatedInput },
+		template: `
+		<validated-form :show-error="showError" error-message="Form submission failed due to a server error.">
+			<validated-input type="text"
+			                 label="First Name"
+			                 :rules="firstNameRules"
+			                 v-model="firstName" />
+
+			<validated-input type="text"
+			                 label="Password"
+			                 :rules="passwordRules"
+			                 v-model="password" />
+			<button style="margin-top:20px" type="submit">SUBMIT</button>
+		</validated-form>
+	`,
+
+		data() {
+			return {
+				firstName: null,
+				password: null,
+				showError: false,
+				firstNameRules: [{
+					validator: isNotEmpty,
+					message: 'Please enter something.',
+				}],
+				passwordRules: [{
+					validator: isNotEmpty,
+					message: 'Please enter something.',
+				}, {
+					validator: hasMinimumLength,
+					message: 'Must be at least 6 characters long.',
+					options: { length: 6 },
+				}],
+			};
+		},
 	};
 }
