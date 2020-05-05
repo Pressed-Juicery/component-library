dd7d33a5d09a4d0b05f56273617a6e4f4aeb55aa
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-05 07:09:35 -0700

    feat(validated select stories): add validated select stories

---
+++ components/validated-select.vue
@@ -0,0 +1,39 @@
+<template>
+	<label class="validated-select">
+		{{ label }}
+		<select class="validated-select__select" v-model="model">
+			<option v-for="(option, index) in options" :key="index" :value="option.value">
+				{{ option.name }}
+			</option>
+		</select>
+	</label>
+</template>
+
+<script>
+	export default {
+		props: {
+			label: String,
+			placeholder: String,
+			options: Array,
+			selectedItem: String,
+		},
+
+		model: {
+			prop: 'selectedItem',
+			event: 'input',
+		},
+
+		computed: {
+			model: {
+				get() { return this.selectedItem },
+				set(value) { this.$emit('input', value) },
+			},
+		},
+	}
+</script>
+
+<style scoped lang="scss">
+	.validated-select__select {
+		display: block;
+	}
+</style>
