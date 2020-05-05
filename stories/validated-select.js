0995dcb1ad4a90cd2f627e0745b040ae15994514
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-05 07:09:16 -0700

    feat(validated select): add validated select component

---
+++ stories/validated-select.js
@@ -0,0 +1,32 @@
+import ValidatedSelect from '../components/validated-select';
+
+export default {
+	title: 'ValidatedSelect',
+	component: ValidatedSelect,
+};
+
+export function Overview() {
+	return {
+		components: { ValidatedSelect },
+		template: `
+			<div>
+				<validated-select label="Label"
+				                  v-model="selectedItem"
+				                  :options="options" />
+				<p>Selected Item: {{ selectedItem }}</p>
+			</div>
+		`,
+
+		data() {
+			return {
+				selectedItem: null,
+				options: [
+					{ name: 'California', value: 'CA'},
+					{ name: 'Oregon', value: 'OR'},
+					{ name: 'Nevada', value: 'NV'}
+				],
+			};
+		},
+	};
+}
+
