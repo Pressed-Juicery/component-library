9b20e2d3330568860e05c5a43afeb5fb02f56e72
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-06 11:50:35 -0700

    feat(address form stories): add v-model tests

--- stories/address-form.js
+++ stories/address-form.js
@@ -11,8 +11,15 @@ export function Overview() {
 
 		template: `
 			<div>
-				<p v-if="address.name">Name: {{ address.name }}</p>
 				<address-form v-model="address" />
+				<div v-if="address.firstName">First Name: {{ address.firstName }}</div>
+				<div v-if="address.lastName">Last Name: {{ address.lastName }}</div>
+				<div v-if="address.streetAddress">Street Address: {{ address.streetAddress }}</div>
+				<div v-if="address.extendedAddress">Extended Address: {{ address.extendedAddress }}</div>
+				<div v-if="address.locality">Locality: {{ address.locality }}</div>
+				<div v-if="address.region">Region: {{ address.region }}</div>
+				<div v-if="address.postal">Postal: {{ address.postal }}</div>
+				<div v-if="address.phone">Phone: {{ address.phone }}</div>
 			</div>
 		`,
 
