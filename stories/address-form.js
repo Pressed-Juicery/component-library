bf503c81567f2b34c418f29c390c2f937f0e86dc
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-06 11:50:57 -0700

    feat(address form stories): add edit address story

--- stories/address-form.js
+++ stories/address-form.js
@@ -26,3 +26,37 @@ export function Overview() {
 		data() { return { address: {} } },
 	};
 }
+
+export function EditAddress() {
+	return {
+		components: { AddressForm },
+
+		template: `
+			<div>
+				<address-form v-model="address" />
+				<div v-if="address.firstName">First Name: {{ address.firstName }}</div>
+				<div v-if="address.lastName">Last Name: {{ address.lastName }}</div>
+				<div v-if="address.streetAddress">Street Address: {{ address.streetAddress }}</div>
+				<div v-if="address.extendedAddress">Extended Address: {{ address.extendedAddress }}</div>
+				<div v-if="address.locality">Locality: {{ address.locality }}</div>
+				<div v-if="address.region">Region: {{ address.region }}</div>
+				<div v-if="address.postal">Postal: {{ address.postal }}</div>
+				<div v-if="address.phone">Phone: {{ address.phone }}</div>
+			</div>
+		`,
+
+		data() {
+			return {
+				address: {
+					firstName: 'Arthur',
+					lastName: 'Dent',
+					streetAddress: '42 High Street',
+					locality: 'Cambridge',
+					region: 'MA',
+					postal: '93742',
+					phone: '559-242-4242',
+				},
+			};
+		},
+	};
+}
