6754a96fcd7ada6eced70c7d5dc9bd92211534da
Spencer <slalexander92@gmail.com>
2020-05-06 09:39:01 -0700

    feat(content-gutter): add content-gutter stories

---
+++ stories/content-gutter.js
@@ -0,0 +1,44 @@
+import ContentGutter from '../components/content-gutter';
+
+export default {
+	title: 'ContentGutter',
+	component: ContentGutter,
+};
+
+export function Overview() {
+	return {
+		components: { ContentGutter },
+		data(){
+			return {
+				backgroundColor: '',
+			}
+		},
+		template: `
+			<content-gutter :backgroundColor="backgroundColor">
+				<div>
+					<h1>This is a content-gutter</h1>
+					<p>The contents are completely customizable.</p>
+				</div>
+			</content-gutter>
+		`,
+	};
+}
+
+export function GutterWithBackgroundColor() {
+	return {
+		components: { ContentGutter },
+		data(){
+			return {
+				backgroundColor: 'blue',
+			}
+		},
+		template: `
+			<content-gutter :backgroundColor="backgroundColor">
+				<div>
+					<h1>This is a content-gutter</h1>
+					<p>The contents are completely customizable.</p>
+				</div>
+			</content-gutter>
+		`,
+	};
+}
