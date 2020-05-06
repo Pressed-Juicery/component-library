d05d2eb9a34e0fc8920cd8ccfa007a3f9f0c0bea
Spencer <slalexander92@gmail.com>
2020-05-06 15:46:39 -0700

    feat(product-card): add product card stories

--- stories/product-card.js
+++ stories/product-card.js
@@ -1,4 +1,72 @@
 import ProductCard from '../components/product-card';
+import ProductCardGrid from '../components/product-card-grid';
+
+const dummyImageURL = 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/bundle-card-berriesandcream.png';
+
+const products = [
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+		secondaryDescription: 'Includes | BlendJet & Citrus 2 (x4)',
+		price: '$55.00',
+		themeColor: '#dbb963',
+		productLink: '#',
+	},
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+		price: '$55.00',
+		themeColor: 'pink',
+		productLink: '#',
+	},
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+		secondaryDescription: 'Includes | BlendJet & Citrus 2 (x4)',
+		price: '$55.00',
+		themeColor: 'purple',
+		productLink: '#',
+	},
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: `aSweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.
+		Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.`,
+		price: '$55.00',
+		themeColor: '#dbb963',
+		productLink: '#',
+	},
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+		secondaryDescription: 'Includes | BlendJet & Citrus 2 (x4)',
+		price: '$55.00',
+		themeColor: 'green',
+		productLink: '#',
+	},
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+		secondaryDescription: 'Includes | BlendJet & Citrus 2 (x4)',
+		price: '$55.00',
+		themeColor: '#dbb963',
+		productLink: '#',
+	},
+	{
+		image: dummyImageURL,
+		title: 'Tropical Glow',
+		description: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+		secondaryDescription: 'Includes | BlendJet & Citrus 2 (x4)',
+		price: '$55.00',
+		themeColor: '#dbb963',
+		productLink: '#',
+	},
+];
 
 export default {
 	title: 'ProductCard',
@@ -8,10 +76,31 @@ export default {
 export function Overview() {
 	return {
 		components: { ProductCard },
-    template: `
-      <div>
-        <product-card />
-      </div>
+		data() {
+			return {
+				product: products[0],
+			};
+		},
+		template: `
+			<div>
+				<product-card :product="product"/>
+			</div>
+		`,
+	};
+}
+
+export function MultipleCards() {
+	return {
+		components: { ProductCardGrid, ProductCard },
+		data() {
+			return {
+				products: products,
+			};
+		},
+		template: `
+			<div>
+				<product-card-grid :products="products" />
+			</div>
 		`,
 	};
 }
