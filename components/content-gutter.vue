ab4803781daf0b4fed35611acc2d6090804de438
Spencer <slalexander92@gmail.com>
2020-05-06 09:38:43 -0700

    feat(content-gutter): add content-gutter component

---
+++ components/content-gutter.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="content-gutter" :style="{ backgroundColor }">
+		<div class="content-gutter__container">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		backgroundColor: String,
+	}
+}
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+
+.content-gutter__container {
+	max-width: $max-width-laptop-large;
+	margin: 0 auto;
+}
+</style>
