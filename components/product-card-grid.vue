f8f9195bd9bc4073573f38dacc3855e8f5e1c5e1
Spencer <slalexander92@gmail.com>
2020-05-06 15:47:55 -0700

    feat(product-card-grid): add product card grid

---
+++ components/product-card-grid.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="product-card-grid">
+		<product-card class="product-card-grid__card"
+		v-for="(product, i) in products"
+		:product="product"
+		:key="'product-' + i" />
+	</div>
+</template>
+
+<script>
+import ProductCard from './product-card';
+
+export default {
+	components: { ProductCard },
+	props: {
+		products: Array,
+	},
+};
+</script>
+
+<style lang="scss">
+	.product-card-grid {
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
