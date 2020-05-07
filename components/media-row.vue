5db948fdfb1e6ce22f93203f3c23db1f9146009e
Spencer <slalexander92@gmail.com>
2020-05-07 11:20:52 -0700

    feat(media-row): add html template

--- components/media-row.vue
+++ components/media-row.vue
@@ -1,5 +1,18 @@
 <template>
-
+	<div class="media-row" :style="{ backgroundColor, color }">
+		<div class="media-row__content">
+			<div class="media-row__content-wrapper">
+				<img class="media-row__icon" :src="icon" />
+				<p class="media-row__title" v-html="title"></p>
+				<p class="media-row__text">
+					<slot />
+				</p>
+			</div>
+		</div>
+		<div class="media-row__image" :style="{'background-image': 'url(' + img + ')'}">
+			<span role="img" :aria-label="alt"></span>
+		</div>
+	</div>
 </template>
 
 <script>
