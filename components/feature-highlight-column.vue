073fd64dd88453158ee83c790ec574feb46a2de2
Spencer <slalexander92@gmail.com>
2020-05-08 12:04:32 -0700

    feat(feature-highlight-column): add props, style component, add stories

--- components/feature-highlight-column.vue
+++ components/feature-highlight-column.vue
@@ -1,17 +1,43 @@
 <template>
-	<div class="feature-highlight-column">
-		<img :src="icon"/>
-		<p>{{ title }}</p>
-		<p v-html="description"></p>
+	<div :class="['feature-highlight-column', { 'feature-highlight-column--centered': centerContent }]" :style="{ color }">
+		<img :src="icon" :style="{ 'fill': color }"/>
+		<p class="feature-highlight-column__title">{{ title }}</p>
+		<p class="feature-highlight-column__description" v-html="description"></p>
 	</div>
 </template>
 
 <script>
 export default {
-
-}
+	props: {
+		icon: String,
+		title: String,
+		description: String,
+		color: String,
+		centerContent: Boolean,
+	},
+};
 </script>
 
 <style lang="scss">
+@import '../styles/variables';
+@import '../styles/mixins';
 
+.feature-highlight-column {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+}
+
+.feature-highlight-column--centered {
+	align-items: center;
+}
+
+.feature-highlight-column img,
+.feature-highlight-column__title {
+	margin-bottom: $spacing-03;
+}
+
+.feature-highlight-column__description {
+	margin-bottom: 0;
+}
 </style>
--- stories/feature-highlight-column.js
+++ stories/feature-highlight-column.js
@@ -9,7 +9,15 @@ export function Overview() {
 	return {
 		components: { FeatureHighlightColumn },
 		template: `
-			<feature-highlight-column />
+			<feature-highlight-column :icon="icon" :title="title" :description="description"/>
 		`,
+		data() {
+			return {
+				icon: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/icons/coconut.svg',
+				title: 'Coconuts',
+				description: 'Coconuts are good for you because of the way that they are.',
+				color: 'red',
+			};
+		},
 	};
 }
