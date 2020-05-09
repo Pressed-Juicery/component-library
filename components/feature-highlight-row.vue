35d50b56b91c0125a9d35170c3696acd126e7b5e
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-09 00:53:01 -0700

    feat(feature-highlight-row): add `feature-highlight-row` component

---
+++ components/feature-highlight-row.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="feature-highlight-row">
+		<feature-highlight-column
+			v-for="(feature, index) in features"
+			:key="index"
+			:icon="feature.icon"
+			:title="feature.title"
+			:description="feature.description"
+		/>
+	</div>
+</template>
+
+<script>
+import FeatureHighlightColumn from './feature-highlight-column';
+
+export default {
+	components: { FeatureHighlightColumn },
+	props: {
+		features: Array,
+	},
+};
+</script>
+
+<style scoped lang="scss">
+	@import '../styles/variables';
+
+	.feature-highlight-row {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-gap: $spacing-10;
+
+		@media (max-width: $max-width-tablet) {
+			grid-template-columns: 1fr;
+			grid-gap: $spacing-07;
+		}
+	}
+</style>
