2f0d19ac6ae78d32629dc33eec82eaa57e074038
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-09 09:43:17 -0700

    style(validators): move functions to beginning of file

--- utilities/validators.js
+++ utilities/validators.js
@@ -1,17 +1,17 @@
-export function hasMinimumLength(value, { length }) {
-	return isEmpty(value) || value.length >= length;
-}
-
-export function isEmail(value) {
-	return isEmpty(value) || /^\S+@\S+\.\S+$/.test(value);
-}
-
 export function isEmpty(value) {
 	return !value && value !== 0;
 }
 
 export function isNotEmpty(value) {
 	return !isEmpty(value);
+}
+
+export function hasMinimumLength(value, { length }) {
+	return isEmpty(value) || value.length >= length;
+}
+
+export function isEmail(value) {
+	return isEmpty(value) || /^\S+@\S+\.\S+$/.test(value);
 }
 
 export function isValidPhoneNumber(value) {
