0d0fb4058ff5d52e3b78fd4bbe279b17a15f4269
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-10 00:21:06 -0700

    refactor(cart-item-card story): use numbers instead of strings

--- stories/cart-item-card.js
+++ stories/cart-item-card.js
@@ -15,8 +15,8 @@ export function Overview() {
 				item: {
 					name: 'Strawberry Basil Lemonade',
 					imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
-					quantity: '3',
-					discountedTotalPrice: '6.50',
+					quantity: 3,
+					discountedTotalPrice: 6.50,
 				},
 			};
 		},
@@ -34,8 +34,8 @@ export function AdditionalInformation() {
 					name: 'Strawberry Almond',
 					imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
 					additionalInformation: 'Chocolate chip cookies, granola, chocolate chips',
-					quantity: '3',
-					discountedTotalPrice: '6.50',
+					quantity: 3,
+					discountedTotalPrice: 6.50,
 				},
 			};
 		},
@@ -56,8 +56,8 @@ export function LongAdditionalInformation() {
 						Chocolate chip cookies, granola, chocolate chips, coconut,
 						blueberries, pistachios, almonds, chocolate sauce, caramel
 					`,
-					quantity: '3',
-					discountedTotalPrice: '6.50',
+					quantity: 3,
+					discountedTotalPrice: 6.50,
 				},
 			};
 		},
