7cba5be94cbf7efc76746a43e87a623f8123eb42
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-18 12:47:40 -0700

    feat(product-card-section): add product-card-section component

---
+++ components/product-card-section.vue
@@ -0,0 +1,42 @@
+<template>
+	<content-wrapper :is-small="true">
+		<div :class="$style.title">{{ config.title }}</div>
+		<div :class="$style.description">{{ config.description }}</div>
+		<product-card-grid :cards="config.cards" />
+	</content-wrapper>
+</template>
+<script>
+	import ContentWrapper from './content-wrapper';
+	import ProductCardGrid from './product-card-grid';
+
+	export default {
+		components: { ProductCardGrid, ContentWrapper },
+		props: {
+			config: {
+				type: Object,
+				required: true,
+			},
+		},
+	};
+</script>
+
+<style module lang="scss">
+	@import '../styles/mixins';
+	@import '../styles/variables';
+
+	.title,
+	.description {
+		max-width: 504px;
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	.title {
+		@include text-heading-4();
+		margin-bottom: $spacing-02;
+	}
+
+	.description {
+		margin-bottom: $spacing-09;
+	}
+</style>
---
+++ stories/product-card-section.js
@@ -0,0 +1,138 @@
+import ProductCardSection from '../components/product-card-section';
+
+const cards = [
+	{
+		product: {
+			imageUrl: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/bundle-card-berriesandcream.png',
+			name: 'Triple Berry',
+			shortDescription: 'This triple berry & banana smoothie recipe is perfect for a healthy refuel between meals.', // eslint-disable-line max-len
+			price: '$55.00',
+			url: '#',
+			bundleItems: [
+				{ name: 'BlendJet' },
+				{ name: 'Vanilla Almond' },
+				{ name: 'Vanilla Almond' },
+				{ name: 'Vanilla Almond' },
+				{ name: 'Vanilla Almond' },
+			],
+		},
+		theme: {
+			color: '#c58582',
+		},
+	},
+	{
+		product: {
+			imageUrl: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/bundle-card-tropicalglow.png',
+			name: 'Tropical Glow',
+			shortDescription: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+			price: '$55.00',
+			url: '#',
+			bundleItems: [
+				{ name: 'BlendJet' },
+				{ name: 'Citrus 2' },
+				{ name: 'Citrus 2' },
+				{ name: 'Citrus 2' },
+				{ name: 'Citrus 2' },
+			],
+		},
+		theme: {
+			color: '#e0c069',
+		},
+	},
+	{
+		product: {
+			imageUrl: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/bundle-card-essentialgreens.png',
+			name: 'Essential Greens',
+			shortDescription: 'All the leafy goodness of greens & cold-pressed juice in one powerhouse smoothie recipe.', // eslint-disable-line max-len
+			price: '$55.00',
+			url: '#',
+			bundleItems: [
+				{ name: 'BlendJet' },
+				{ name: 'Greens 2' },
+				{ name: 'Greens 2' },
+				{ name: 'Greens 2' },
+				{ name: 'Greens 2' },
+			],
+		},
+		theme: {
+			color: '#50561a',
+		},
+	},
+	{
+		product: {
+			imageUrl: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/bundle-card-powerprotein.png',
+			name: 'Power Protein',
+			shortDescription: 'This chocolate peanut butter smoothie recipe is perfect for a post-workout treat.',
+			price: '$55.00',
+			url: '#',
+			bundleItems: [
+				{ name: 'BlendJet' },
+				{ name: 'Chocolate Almond' },
+				{ name: 'Chocolate Almond' },
+				{ name: 'Chocolate Almond' },
+				{ name: 'Chocolate Almond' },
+			],
+		},
+		theme: {
+			color: '#af7340',
+		},
+	},
+	{
+		product: {
+			imageUrl: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/bundle-card-variety.png',
+			name: 'Variety Pack',
+			shortDescription: 'Sweet peach and mango meet citrus flavors in this crowd-pleaser smoothie recipe.',
+			price: '$55.00',
+			url: '#',
+			bundleItems: [
+				{ name: 'BlendJet' },
+				{ name: 'Greens 2' },
+				{ name: 'Citrus 2' },
+				{ name: 'Vanilla Almond' },
+				{ name: 'Chocolate Almond' },
+			],
+		},
+		theme: {
+			color: '#3f3d62',
+		},
+	},
+	{
+		product: {
+			imageUrl: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/pages/blendjet-landing/blendjet-blender.png',
+			name: 'BlendJet',
+			shortDescription: 'Small, but mighty powerful. Meet the modern day blender for a quick nutritious smoothie.', // eslint-disable-line max-len
+			price: '$39.95',
+			url: '#',
+			bundleItems: [
+				{ name: 'BlendJet' },
+			],
+		},
+		theme: {
+			color: '#282321',
+		},
+	},
+];
+
+export default {
+	title: 'ProductCardSection',
+	component: ProductCardSection,
+};
+
+export function Overview() {
+	return {
+		components: { ProductCardSection },
+		template: '<product-card-section :config="config" />',
+		data() {
+			return {
+				config: {
+					title: 'Choose Your Bundle',
+					description: `
+						Each bundle includes a BlendJet, a recipe guide and nutritious cold-pressed juices.
+						Simply combine your fruit, veggie or protein ingredients, add your Pressed juice, blend and go!
+					`,
+					cards,
+				}
+			};
+		},
+	};
+}
