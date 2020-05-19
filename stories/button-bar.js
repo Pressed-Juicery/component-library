031abf029b31d367d82cb4754793bd26e6448932
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-19 00:09:51 -0700

    feat(button-bar): rewrite to follow new design requirements

--- stories/button-bar.js
+++ stories/button-bar.js
@@ -1,5 +1,4 @@
 import ButtonBar from '../components/button-bar';
-import ButtonBarButton from '../components/button-bar-button';
 
 export default {
 	title: 'ButtonBar',
@@ -8,147 +7,18 @@ export default {
 
 export function Overview() {
 	return {
-		components: { ButtonBar, ButtonBarButton },
+		components: { ButtonBar },
 		template: `
-			<button-bar>
-				<button-bar-button style="padding:20px"
-				                   value="Hello World"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div>Hello World</div>
-				</button-bar-button>
-				<button-bar-button style="padding:20px"
-				                   value="Guten Tag"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div>Guten Tag</div>
-				</button-bar-button>
-				<button-bar-button style="padding:20px"
-				                   value="Bonjour Tout le Monde"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div>Bonjour Tout le Monde</div>
-				</button-bar-button>
-			</button-bar>
+			<button-bar
+				:button-labels="['Pick Up', 'Delivery', 'Shipping']"
+				selected-label="Pick Up"
+				@change="onChange"
+			/>
 		`,
-
-		data() { return { selectedItem: null } },
-	};
-}
-
-export function LoopedContent() {
-	return {
-		components: { ButtonBar, ButtonBarButton },
-		template: `
-			<button-bar>
-				<button-bar-button style="padding:20px"
-				                   v-for="(content, index) in contents"
-				                   :key="index"
-				                   :value="content"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div>{{ content.title }}</div>
-					<div>{{ content.detail }}</div>
-				</button-bar-button>
-			</button-bar>
-		`,
-
-		data() {
-			return {
-				contents: [
-					{ title: 'Hello', detail: 'World' },
-					{ title: 'Guten', detail: 'Tag' },
-					{ title: 'Bonjour', detail: 'Tout le Monde' },
-					{ title: '¡Hola', detail: 'Mundo!' },
-				],
-				selectedItem: null,
-			};
+		methods: {
+			onChange(value) {
+				console.log(`Selected button changed: ${value}`); // eslint-disable-line no-console
+			},
 		},
-	};
-}
-
-export function VariedContent() {
-	return {
-		components: { ButtonBar, ButtonBarButton },
-		template: `
-			<button-bar>
-				<button-bar-button style="padding:20px"
-				                   v-for="(content, index) in contents"
-				                   :key="index"
-				                   :value="content"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div v-if="content.primary" style="font-weight:bold">{{ content.primary }}</div>
-					<div v-if="content.secondary">{{ content.secondary }}</div>
-					<div v-if="content.tertiary">{{ content.tertiary }}</div>
-				</button-bar-button>
-			</button-bar>
-		`,
-
-		data() {
-			return {
-				contents: [
-					{ primary: 'Primary' },
-					{ primary: 'Primary', secondary: 'Secondary', tertiary: 'Tertiary' },
-					{ secondary: 'Secondary', tertiary: 'Tertiary' },
-				],
-				selectedItem: null,
-			};
-		},
-	};
-}
-
-export function LongerContent() {
-	return {
-		components: { ButtonBar, ButtonBarButton },
-		template: `
-			<button-bar>
-				<button-bar-button style="padding:20px"
-				                   v-for="(content, index) in contents"
-				                   :key="index"
-				                   :value="content"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div v-if="content.primary" style="font-weight:bold">{{ content.primary }}</div>
-					<div v-if="content.secondary">{{ content.secondary }}</div>
-					<div v-if="content.tertiary">{{ content.tertiary }}</div>
-				</button-bar-button>
-			</button-bar>
-		`,
-
-		data() {
-			return {
-				contents: [
-					{ primary: 'Primary', secondary: 'Secondary' },
-					{ secondary: 'This is excessively longer button content.' },
-					{ primary: 'Primary', secondary: 'Secondary' },
-				],
-				selectedItem: null,
-			};
-		},
-	};
-}
-
-export function PreSelected() {
-	return {
-		components: { ButtonBar, ButtonBarButton },
-		template: `
-			<button-bar>
-				<button-bar-button style="padding:20px"
-				                   value="Hello World"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div>Hello World</div>
-				</button-bar-button>
-				<button-bar-button style="padding:20px"
-				                   value="Bonjour Tout le Monde"
-				                   :selectedButton="selectedItem"
-				                   @select-button="value => selectedItem = value">
-					<div>Bonjour Tout le Monde</div>
-				</button-bar-button>
-			</button-bar>
-		`,
-
-		data() { return { selectedItem: 'Hello World' } },
 	};
 }
