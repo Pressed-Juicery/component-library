f10925bb14d0d0bcc1f77ffe8781bb56532de582
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-19 14:26:27 -0700

    refactor(feature-highlight-row story): use data from demo data file

--- stories/components/sections/feature-highlight-row.js
+++ stories/components/sections/feature-highlight-row.js
@@ -1,40 +1,17 @@
 import FeatureHighlightRow from '../../../components/feature-highlight-row';
+import { featureHighlightData } from '../../../demo-data/feature-highlight.data';
 
 export default {
 	title: 'Sections / FeatureHighlightRow',
 	component: FeatureHighlightRow,
 };
 
-// eslint-disable-next-line max-lines-per-function
 export function Overview() {
 	return {
 		components: { FeatureHighlightRow },
-		template: `
-			<feature-highlight-row :features="features" />
-		`,
+		template: '<feature-highlight-row :features="features" />',
 		data() {
-			return {
-				features: [{
-					icon: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/icons/coconut.svg',
-					title: 'Coconuts',
-					description: `
-						Coconuts are good for you because of the way that they are.
-						Coconuts are good for you because of the way that they are.
-					`,
-				}, {
-					icon: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/icons/coconut.svg',
-					title: 'Coconuts',
-					description: `
-						Coconuts are good for you because of the way that they are.
-						Coconuts are good for you because of the way that they are.
-						Coconuts are good for you because of the way that they are.
-					`,
-				}, {
-					icon: 'https://pressed-product-images.s3-us-west-1.amazonaws.com/shopify/icons/coconut.svg',
-					title: 'Coconuts',
-					description: 'Coconuts are good for you because of the way that they are.',
-				}],
-			};
+			return { features: featureHighlightData.features };
 		},
 	};
 }
