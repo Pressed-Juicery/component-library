e466975df929bee61d218bba0adb0a7c64d32f7a
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-19 23:49:25 -0700

    feat(landing-page-template): add `landing-page-template`

---
+++ demo-data/landing-page-template.data.js
@@ -0,0 +1,9 @@
+import { bundleCardSectionData as bundleCardSection } from './bundle-card-section.data.js';
+import { highlightSectionData as highlightSection } from './highlight-section.data.js';
+import { imageBannerData as imageBannerSection } from './image-banner.data.js';
+
+export const landingPageTemplateData = {
+	imageBannerSection,
+	highlightSection,
+	bundleCardSection,
+};
