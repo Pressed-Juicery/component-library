01f28ce002e47bdfb7e5513f4103ff507fa5ac60
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-22 08:17:56 -0700

    feat(cart totals): add cart totals stories

---
+++ stories/components/cart/cart-totals.js
@@ -0,0 +1,69 @@
+import CartTotals from '../../../components/cart-totals';
+
+export default {
+	title: 'Components / Cart / CartTotals',
+	component: CartTotals,
+};
+
+const cart = {
+	subtotal: 100,
+	total: 150.5,
+	shippingPrice: 40,
+	discounts: [
+		{ name: 'VIP Member Savings', amount: 2.5 },
+		{ name: 'Points Redemption', amount: 6.5 },
+	],
+};
+
+export function Overview() {
+	return {
+		components: { CartTotals },
+		template: '<cart-totals :cart="cart" style="margin:40px" />',
+
+		data() {
+			return {
+				cart: {
+					subtotal: 100,
+					total: 150.5,
+				},
+			};
+		}
+	};
+}
+
+export function ShippingDeliveryPrice() {
+	return {
+		components: { CartTotals },
+		template: '<cart-totals :cart="cart"  style="margin:40px" />',
+
+		data() {
+			return {
+				cart: {
+					subtotal: 100,
+					total: 150.5,
+					shippingPrice: 40,
+				},
+			};
+		},
+	};
+}
+
+export function Discounts() {
+	return {
+		components: { CartTotals },
+		template: '<cart-totals :cart="cart"  style="margin:40px" />',
+
+		data() {
+			return {
+				cart: {
+					subtotal: 100,
+					total: 150.5,
+					discounts: [
+						{ name: 'VIP Member Savings', amount: 2.5 },
+						{ name: 'Points Redemption', amount: 6.5 },
+					],
+				},
+			};
+		},
+	};
+}
