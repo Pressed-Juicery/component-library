57823d2e5e0d5fef03327b150245cd592e88bbb2
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-22 08:17:29 -0700

    feat(cart totals): add cart totals component

---
+++ components/cart-totals.vue
@@ -0,0 +1,102 @@
+<template>
+	<div>
+		<div :class="[$style.row]">
+			<div :class="[$style.subtotalToggle, { [$style.isOpen]: isOpen }]" @click="toggle()">
+				<div>Subtotal</div>
+				<arrow-down-icon v-if="cart.discounts && cart.discounts.length" :class="$style.icon" />
+			</div>
+			<div>{{ formatCurrency(cart.subtotal) }}</div>
+		</div>
+
+		<div v-if="isOpen && cart.discounts && cart.discounts.length"
+		     :class="[$style.row]"
+		     v-for="discount in cart.discounts"
+		     :key="discount.name">
+			<div :class="$style.discountLabel">{{ discount.name }}</div>
+			<div :class="$style.discountAmount">{{ formatCurrency(-Math.abs(discount.amount)) }}</div>
+		</div>
+
+		<div :class="[$style.row]">
+			<div>Shipping/Delivery</div>
+			<div v-if="cart.shippingPrice">{{ formatCurrency(cart.shippingPrice) }}</div>
+			<div v-else :class="$style.shippingInfo">calculated at next step</div>
+		</div>
+
+		<div :class="$style.row">
+			<div :class="$style.totalLabel">Estimated Total</div>
+			<div :class="$style.total">{{ formatCurrency(cart.total) }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import ArrowDownIcon from './icons/arrow-down-icon';
+	import { formatCurrency } from '../utilities/formatters';
+
+	export default {
+		components: { ArrowDownIcon },
+
+		props: {
+			cart: Object,
+		},
+
+		data() {
+			return {
+				isOpen: false,
+			};
+		},
+
+		methods: {
+			formatCurrency(amount) {
+				return formatCurrency(amount);
+			},
+
+			toggle() {
+				this.isOpen = !this.isOpen;
+			},
+		},
+	}
+</script>
+
+<style module lang="scss">
+	@import '../styles/mixins';
+	@import '../styles/variables';
+
+	.row {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: $spacing-03 0;
+	}
+
+	.subtotalToggle {
+		display:flex;
+		align-items: center;
+		cursor: pointer;
+	}
+
+	.icon {
+		width: $spacing-04;
+		fill: $gray-90;
+		margin-left: $spacing-03;
+	}
+
+	.isOpen {
+		.icon {
+			transform: rotate(180deg);
+		}
+	}
+
+	.shippingInfo {
+		@include text-body-small();
+	}
+
+	.discountLabel,
+	.discountAmount {
+		@include text-subtle();
+	}
+
+	.totalLabel, .total {
+		@include text-bolder();
+	}
+</style>
