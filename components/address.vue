b2a56ca028aed0980acc27349243a5b9c6ad7619
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-22 19:41:55 -0700

    feat(address): add text styles

--- components/address.vue
+++ components/address.vue
@@ -1,21 +1,34 @@
 <template>
-	<div v-if="name" :class="$style.name">{{ address.name }}</div>
+	<div>
+		<div v-if="address.name" :class="$style.title">{{ address.name }}</div>
 
-	<div>{{ address.streetAddress }}</div>
-	<div v-if="address.extendedAddress">{{ address.extendedAddress }}</div>
-	<div>{{ address.locality }}, {{ address.region }} {{ address.postal }}</div>
+		<div :class="$style.text">{{ address.streetAddress }}</div>
+		<div :class="$style.text" v-if="address.extendedAddress">{{ address.extendedAddress }}</div>
+		<div :class="$style.text">{{ address.locality }}, {{ address.region }} {{ address.postal }}</div>
 
-	<div v-if="address.phone">{{ address.phone }}</div>
+		<div :class="$style.text" v-if="address.phone">{{ address.phone }}</div>
+	</div>
 </template>
 
 <script>
 export default {
 	props: {
-		address: Object,
+		address: { required: true },
 	},
 };
 </script>
 
 <style module lang="scss">
+	@import '../styles/mixins';
+	@import '../styles/variables';
 
+	.title {
+		@include text-bold();
+
+		margin-bottom: $spacing-02;
+	}
+
+	.text {
+		@include text-body-small();
+	}
 </style>
