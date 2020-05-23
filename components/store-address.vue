e07d6347d28b0de9ecc8bac43530cd5321078a50
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-22 20:08:43 -0700

    feat(store address): add store address component

---
+++ components/store-address.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<Address :address="store.address" />
+		<div v-if="store.storeHours" :class="$style.details">
+			<div v-for="(hours, index) in store.storeHours" :key="index">{{ hours }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Address from './address.vue';
+
+export default {
+	components: { Address },
+
+	props: {
+		store: {
+			required: true,
+		},
+	},
+}
+</script>
+
+<style module lang="scss">
+	@import '../styles/mixins';
+
+	.details {
+		@include text-body-small();
+	}
+</style>
