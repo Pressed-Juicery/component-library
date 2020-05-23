ecfc6055d595e52d8a7fbb17e1be4659fe2031d0
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-22 18:48:58 -0700

    feat(address): add address component

---
+++ components/address-component.vue
@@ -0,0 +1,17 @@
+<template>
+	<div v-if="name" :class="$style.name">{{ address.name }}</div>
+
+	<div>{{ address.streetAddress }}</div>
+	<div v-if="address.extendedAddress">{{ address.extendedAddress }}</div>
+	<div>{{ address.locality }}, {{ address.region }} {{ address.postal }}</div>
+
+	<div v-if="address.phone">{{ address.phone }}</div>
+</template>
+
+<script>
+export default {
+	props: {
+		address: Object,
+	},
+};
+</script>
