8c875efc05aae72931e4933aa66462e7c32fb0f6
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-23 19:30:02 -0700

    refactor(address form): use css modules syntax

--- components/address-form.vue
+++ components/address-form.vue
@@ -1,6 +1,6 @@
 <template>
-	<validated-form :id="formId" class="address-form" @submit="$emit('submit', address)">
-		<div class="address-form__grid">
+	<validated-form :id="formId" @submit="$emit('submit', address)">
+		<div :class="$style.grid">
 			<validated-input label="First Name" v-model="address.firstName" :rules="firstNameRules" />
 			<validated-input label="Last Name" v-model="address.lastName" :rules="lastNameRules" />
 		</div>
@@ -10,19 +10,18 @@
 
 		<validated-input label="City" v-model="address.locality" :rules="localityRules" />
 
-		<div class="address-form__grid">
+		<div :class="$style.grid">
 			<validated-select label="State" v-model="address.region" :options="regions" :rules="regionRules" />
 			<validated-input label="Zip Code" v-model="address.postal" :rules="postalRules" />
 		</div>
 
-		<validated-input class="address-form__phone"
-		                 type="phone"
+		<validated-input type="phone"
 		                 label="Phone Number"
 		                 v-model="address.phone"
 		                 :rules="phoneRules" />
 
 		<Label v-if="showDeliveryInstructions">
-			<span class="address-form__delivery-instructions-label">Delivery Instructions</span>
+			<span :class="$style.deliveryInstructionsLabel">Delivery Instructions</span>
 			<textarea v-model="address.deliveryInstructions"
 			          rows="4"
 			          placeholder="i.e. gate code or if it should be left at your door, etc." />
@@ -105,17 +104,17 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
+<style module lang="scss">
 	@import '../styles/mixins';
 	@import '../styles/variables';
 
-	.address-form__grid {
+	.grid {
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
 		grid-gap: $spacing-05;
 	}
 
-	.address-form__delivery-instructions-label {
+	.deliveryInstructionsLabel {
 		@include text-bolder();
 	}
 </style>
