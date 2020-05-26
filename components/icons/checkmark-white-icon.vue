8caa49392a0f72935e4f35aa2b34d98e82245afc
Silas Langley <silas.langley@pressedjuicery.com>
2020-05-26 11:47:38 -0700

    feat(checkmark white icon): add checkmark white icon component

---
+++ components/icons/checkmark-white-icon.vue
@@ -0,0 +1,27 @@
+<template>
+	<svg width="11px" height="9px" viewBox="0 0 11 9" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		<!-- Generator: Sketch 63.1 (92452) - https://sketch.com -->
+		<title>Group 3</title>
+		<desc>Created with Sketch.</desc>
+		<defs>
+			<polygon id="path-1" points="0 0.294117647 11 0.294117647 11 8.70588235 0 8.70588235"></polygon>
+		</defs>
+		<g id="Ordering-page" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+			<g id="pick-up-selected" transform="translate(-356.000000, -586.000000)">
+				<g id="Group-3" transform="translate(356.000000, 586.000000)">
+					<mask id="mask-2" fill="white">
+						<use xlink:href="#path-1"></use>
+					</mask>
+					<g id="Clip-2"></g>
+					<path d="M10.6642668,0.637627369 C10.2167626,0.179614406 9.4909253,0.179614406 9.04334471,0.637627369 L3.9249492,5.87442995 L1.95681773,3.86076845 C1.50923715,3.40275549 0.783399802,3.40275549 0.335742772,3.86076845 C-0.111914257,4.3187032 -0.111914257,5.0613318 0.335742772,5.51934476 L3.11441172,8.36229442 C3.33831668,8.59122269 3.63163294,8.70588235 3.92487276,8.70588235 C4.21811258,8.70588235 4.51158173,8.5913009 4.7353338,8.36229442 L10.6641903,2.29628189 C11.1119238,1.83819072 11.1119238,1.09556212 10.6642668,0.637627369" id="Fill-1" fill="#FFFFFF" mask="url(#mask-2)"></path>
+				</g>
+			</g>
+		</g>
+	</svg>
+</template>
+
+<script>
+	export default {
+		name: 'checkmark-white-icon',
+	}
+</script>
