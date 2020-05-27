66bc4436753cbf1dd50c666aded98dce4a434965
Spencer <slalexander92@gmail.com>
2020-05-27 11:36:38 -0700

    feat(points-redemption-card): add points-redemption-card

---
+++ components/points-redemption-card.vue
@@ -0,0 +1,59 @@
+<template>
+	<card :class="$style.card">
+		<img :class="$style.icon" :src="icon"/>
+		<div :class="$style.title">{{ title }}</div>
+		<div :class="$style.points">{{ points }}<span :class="$style.pointsIndicator">Pts</span></div>
+	</card>
+</template>
+
+<script>
+import Card from './card';
+
+export default {
+	components: { Card },
+
+	props: {
+		icon: String,
+		title: String,
+		points: String,
+	},
+
+	methods: {
+		handleChange() {
+			const { title, points, quantity } = this;
+
+			this.$emit('change', { title, points, quantity });
+		},
+	},
+};
+</script>
+
+<style module lang="scss" >
+	@import '../styles/mixins';
+
+	.card {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: $spacing-07 $spacing-06;
+	}
+
+	.icon {
+		height: $spacing-09;
+		margin-bottom: $spacing-03;
+	}
+
+	.title {
+		margin-bottom: $spacing-02;
+		text-align: center;
+	}
+
+	.points {
+		@include text-heading-6;
+	}
+
+	.pointsIndicator {
+		margin-left: $spacing-02;
+	}
+
+</style>
