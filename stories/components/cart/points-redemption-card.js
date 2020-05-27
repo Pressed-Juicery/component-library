9c4d24e878de55c93147d0f14079aa8e58f7154d
Spencer <slalexander92@gmail.com>
2020-05-27 11:37:27 -0700

    feat(points-redemption-card): add stories for points-redemption-card

---
+++ stories/components/cart/points-redemption-card.js
@@ -0,0 +1,88 @@
+import PointsRedemption from '../../../components/points-redemption';
+import PointsRedemptionCard from '../../../components/points-redemption-card';
+
+export default {
+	title: 'Points Redemption Card',
+	component: PointsRedemptionCard,
+};
+
+const cards = [
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/water-and-shot.svg',
+		title: 'Shot or Water',
+		points: '70',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/freeze-size-1.svg',
+		title: 'Size 1 Freeze',
+		points: '140',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/juice.svg',
+		title: 'Any Juice',
+		points: '150',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/signature-blend.svg',
+		title: 'Signature Blend',
+		points: '180',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/freeze-size-2.svg',
+		title: 'Size 2 Freeze',
+		points: '180',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/water-and-shot.svg',
+		title: 'Shot or Water',
+		points: '70',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/freeze-size-1.svg',
+		title: 'Size 1 Freeze',
+		points: '140',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/juice.svg',
+		title: 'Any Juice',
+		points: '150',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/signature-blend.svg',
+		title: 'Signature Blend',
+		points: '180',
+	},
+	{
+		icon: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/pressed-points/freeze-size-2.svg',
+		title: 'Size 2 Freeze',
+		points: '180',
+	},
+];
+
+export function Overview() {
+	return {
+		components: { PointsRedemptionCard },
+		template: '<points-redemption-card :icon="icon" :title="title" :points="points" @change="handleChange" />',
+
+		data() {
+			return {
+				icon: cards[0].icon,
+				points: cards[0].points,
+				title: cards[0].title,
+			};
+		},
+	};
+}
+
+export function CardRow() {
+	return {
+		components: { PointsRedemption },
+		template: '<points-redemption :cards="cards" />',
+
+		data() {
+			return {
+				cards: cards,
+			};
+		},
+	};
+}
