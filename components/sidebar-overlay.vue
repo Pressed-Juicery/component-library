8e84e559bd5547a4e08fe2c1949c447c51d0e328
Spencer <slalexander92@gmail.com>
2020-05-28 16:58:32 -0700

    feat(sidebar-overlay): add overlay + sidebar divs

--- components/sidebar-overlay.vue
+++ components/sidebar-overlay.vue
@@ -1,5 +1,9 @@
 <template>
+	<div :class="$style.overlay">
+		<div :class="$style.sidebar">
 
+		</div>
+	</div>
 </template>
 
 <script>
@@ -9,5 +13,23 @@ export default {
 </script>
 
 <style module lang="scss">
+	@import '../styles/mixins';
+
+	.overlay {
+		position: relative;
+		height: 100vh;
+		width: 100%;
+		z-index: -1;
+		background-color: rgba(0, 0, 0, 0.25);
+		opacity: 0.55;
+	}
+
+	.sidebar {
+		position: absolute;
+		right: 0;
+		height: 100%;
+		width: 350px;
+		background-color: $beige;
+	}
 
 </style>
