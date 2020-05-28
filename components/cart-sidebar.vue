e9e19784cf877d2b1357be457044c79f7123da7d
Spencer <slalexander92@gmail.com>
2020-05-28 16:57:41 -0700

    feat(cart-sidebar): add cart-sidebar component template

---
+++ components/cart-sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <sidebar-overlay>
+
+  </sidebar-overlay>
+</template>
+
+<script>
+import SidebarOverlay from './sidebar-overlay';
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
