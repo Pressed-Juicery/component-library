dab086e6c74e7e762dcf49783e0c297eddcd1e7d
Spencer <slalexander92@gmail.com>
2020-05-29 16:53:16 -0700

    feat(cart-sidebar): add cart-sidebar component

--- components/cart-sidebar.vue
+++ components/cart-sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <sidebar-overlay>
+  <sidebar-overlay :active="active" :shouldShowBackButton="shouldShowBackButton" @close="closeSidebar">
 
   </sidebar-overlay>
 </template>
@@ -7,7 +7,23 @@
 <script>
 import SidebarOverlay from './sidebar-overlay';
 export default {
-
+  components: { SidebarOverlay },
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data(){
+    return {
+      shouldShowBackButton: false,
+    }
+  },
+  methods: {
+    closeSidebar() {
+      this.active = !this.active;
+    }
+  }
 }
 </script>
 
