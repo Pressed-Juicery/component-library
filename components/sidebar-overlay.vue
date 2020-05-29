9912d01f26deca6f7f008e1cdb4f64611fc33021
Spencer <slalexander92@gmail.com>
2020-05-29 16:57:30 -0700

    style(cart-sidebar): make linter happy

--- components/sidebar-overlay.vue
+++ components/sidebar-overlay.vue
@@ -3,26 +3,45 @@
 		<div :class="$style.sidebar">
 			<div :class="[$style.sidebarNavigation, {[$style.noBackButton] : !shouldShowBackButton}]">
 
-				<svg v-if="shouldShowBackButton" :class="$style.backButton" @click="$emit('back')" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<svg v-if="shouldShowBackButton" :class="$style.backButton" @click="$emit('back')"
+				version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 					<g id="Web-" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 						<g id="Web---Pressed-Points" transform="translate(-850.000000, -50.000000)" stroke="#000000">
 							<g id="back-button" transform="translate(850.000000, 50.000000)">
 								<circle id="Oval" stroke-width="1.5" cx="11.5" cy="11.5" r="10.75"></circle>
-								<g id="Group" stroke-width="1" fill-rule="evenodd" transform="translate(8.000000, 6.000000)" stroke-linecap="round" stroke-linejoin="round">
-									<line x1="-1.08357767e-13" y1="5.07467221" x2="5.07467221" y2="0" id="Path" stroke-width="2"></line>
-									<line x1="0.00172051948" y1="5.07725299" x2="5.07639273" y2="10.1519252" id="Path" stroke-width="2"></line>
+								<g id="Group" stroke-width="1" fill-rule="evenodd"
+								transform="translate(8.000000, 6.000000)"
+								stroke-linecap="round" stroke-linejoin="round">
+									<line x1="-1.08357767e-13"
+									y1="5.07467221"
+									x2="5.07467221"
+									y2="0" id="Path" stroke-width="2"></line>
+									<line x1="0.00172051948"
+									y1="5.07725299"
+									x2="5.07639273"
+									y2="10.1519252" id="Path" stroke-width="2"></line>
 								</g>
 							</g>
 						</g>
 					</g>
 				</svg>
 
-				<svg :class="$style.closeButton" @click="$emit('close')" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-					<g id="Web-" stroke="none" stroke-width="1" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-						<g id="Web---Pressed-Points" transform="translate(-1206.000000, -55.000000)" stroke="#000000" stroke-width="2">
+				<svg :class="$style.closeButton" @click="$emit('close')"
+				version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+					<g id="Web-"
+					stroke="none"
+					stroke-width="1"
+					fill-rule="evenodd"
+					stroke-linecap="round"
+					stroke-linejoin="round">
+						<g id="Web---Pressed-Points"
+						transform="translate(-1206.000000, -55.000000)"
+						stroke="#000000" stroke-width="2">
 							<g id="Group-7" transform="translate(1206.000000, 56.000000)">
-								<line x1="0.998494545" y1="9.55740364" x2="9.43883273" y2="0.557403636" id="Path"></line>
-								<line x1="0.998494545" y1="0.557403636" x2="9.44033818" y2="9.44033818" id="Path"></line>
+								<line x1="0.998494545"
+								y1="9.55740364" x2="9.43883273" y2="0.557403636" id="Path"></line>
+								<line x1="0.998494545"
+								y1="0.557403636" x2="9.44033818" y2="9.44033818" id="Path"></line>
 							</g>
 						</g>
 					</g>
@@ -40,7 +59,7 @@ export default {
 		active: Boolean,
 		shouldShowBackButton: Boolean,
 	},
-}
+};
 </script>
 
 <style module lang="scss">
