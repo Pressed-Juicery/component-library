e32f00969626e0a089074fff7f00456d745ec51b
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-05-29 18:02:45 -0700

    feat(quantity-selector stories): add missing stories

--- stories/components/basic/quantity-selector.js
+++ stories/components/basic/quantity-selector.js
@@ -18,3 +18,24 @@ export function DropdownOnly() {
 		template: '<quantity-selector :canUseInput="false" />',
 	};
 }
+
+export function CustomRangeWithInput() {
+	return {
+		components: { QuantitySelector },
+		template: '<quantity-selector :options="[0, 1, 2, 3, 4, 5]" />',
+	};
+}
+
+export function CustomRangeWithoutInput() {
+	return {
+		components: { QuantitySelector },
+		template: '<quantity-selector :options="[0, 1, 2, 3, 4, 5]" :canUseInput="false" />',
+	};
+}
+
+export function Preselected() {
+	return {
+		components: { QuantitySelector },
+		template: '<quantity-selector :quantity="3" />',
+	};
+}
