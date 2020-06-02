52497377886218451698d56e2640e2f18e89ecd4
Spencer <slalexander92@gmail.com>
2020-06-02 15:48:15 -0700

    feat(cart-points-display): style points display

--- components/cart-points-display.vue
+++ components/cart-points-display.vue
@@ -1,13 +1,33 @@
 <template>
-
+  <div>
+    <div :class="$style.balanceWrapper">
+      <div>Pressed Points Balance</div>
+      <div>{{ points || 0 }}</div>
+    </div>
+    <button :class="$style.redeemPoints">Redeem Pressed Points</button>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+	props: {
+		points: Number,
+	},
+};
 </script>
 
 <style module lang="scss">
+  @import '../styles/variables';
 
+	.balanceWrapper {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: $spacing-05;
+	}
+
+	.redeemPoints {
+		background-color: $gray-30;
+		color: black;
+		width: 100%;
+	}
 </style>
