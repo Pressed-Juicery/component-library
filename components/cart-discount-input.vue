c5c5fe0c31411cb8215fb7201e5b55505ffb6c62
Spencer <slalexander92@gmail.com>
2020-06-02 15:17:24 -0700

    feat(cart-discount-input): create component for the cart discount input

---
+++ components/cart-discount-input.vue
@@ -0,0 +1,47 @@
+<template>
+	<div :class="[$style.root, {[$style.isEmpty]: !value}]">
+		<input placeholder="Discount Code" :class="$style.input" v-model="value"/>
+		<button :class="$style.button">Apply</button>
+	</div>
+</template>
+
+<script>
+// import ValidatedInput from './validated-input';
+
+export default {
+	// components: { ValidatedInput },
+	props: {
+		placeholder: {
+			type: String,
+		},
+	},
+	data(){
+		return {
+			value: '',
+		};
+	},
+};
+</script>
+
+<style module lang="scss">
+	@import '../styles/buttons';
+
+	.root {
+		display: flex;
+		align-items: center;
+	}
+
+	.root.isEmpty .button{
+		background-color: $gray-30;
+		color: $gray-100;
+	}
+
+	.input {
+		margin-right: $spacing-03;
+	}
+
+	.input::placeholder {
+		color: $gray-30;
+	}
+
+</style>
