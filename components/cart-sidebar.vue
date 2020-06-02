16eff5629e183150cda672906fb3405ab2828ed5
Spencer <slalexander92@gmail.com>
2020-06-02 15:49:06 -0700

    feat(cart-sidebar): assemble cart-sidebar using all of the components that were built previously

--- components/cart-sidebar.vue
+++ components/cart-sidebar.vue
@@ -1,13 +1,56 @@
 <template>
+	<sidebar-overlay :active="active" @close="$emit('close')">
 
+		<cart-navigation slot="navigation" @close="$emit('close')" />
+
+		<div>
+			<cart-summary :class="$style.cartSummary" :cart="cart" />
+			<cart-discount-input @submit="$emit('submit-discount')"/>
+			<hr :class="$style.partition">
+			<cart-points-display />
+		</div>
+
+		<cart-checkout-footer slot="footer" :cart="cart" @checkout="$emit('checkout')"/>
+
+	</sidebar-overlay>
 </template>
 
 <script>
-export default {
+import CartNavigation from './cart-navigation';
+import SidebarOverlay from './sidebar-overlay';
+import CartSummary from './cart-summary';
+import CartDiscountInput from './cart-discount-input';
+import CartPointsDisplay from './cart-points-display';
+import CartCheckoutFooter from './cart-checkout-footer';
 
-}
+export default {
+	components: {
+		CartNavigation,
+		SidebarOverlay,
+		CartSummary,
+		CartDiscountInput,
+		CartPointsDisplay,
+		CartCheckoutFooter,
+	},
+	props: {
+		active: Boolean,
+		cart: Object,
+		user: Object,
+	},
+};
 </script>
 
 <style module lang="scss">
+	@import '../styles/buttons';
+
+	.cartSummary {
+		margin-top: $spacing-07;
+		margin-bottom: $spacing-05;
+	}
+
+	.partition {
+		border: 1px solid $gray-30;
+		margin: $spacing-05 0;
+	}
 
 </style>
