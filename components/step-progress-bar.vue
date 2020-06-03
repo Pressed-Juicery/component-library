b9c50c1589abf7b16946fc21c62237a2d2627a4f
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-02 21:06:27 -0700

    Completed state handling and component styles

--- components/step-progress-bar.vue
+++ components/step-progress-bar.vue
@@ -1,140 +1,117 @@
 <template>
-  <div class="box">
-    <div class="line" />
-    <div
-      :class="['step-cont', {current: index === currentIndex}]"
-      v-for="(step, index) in states"
-      :key="index"
-    >
-      <div class="bubble-box">
-        <!-- Refer to a computed property for styles -->
-        <span
-          :class="{
-          circle: index >= currentIndex,
-          dot: index <= currentIndex
-          }"
-          @click="changeState(index)"
-        ></span>
-      </div>
-      <p>{{ states[index].name }}</p>
-    </div>
-  </div>
+	<div :class="$style.box">
+		<div :class="$style.line" />
+		<div
+			:class="[$style.stepContainer, {[$style.current]: index === currentIndex}]"
+			v-for="(step, index) in states"
+			:key="index"
+		>
+			<div :class="$style.bubbleBox">
+				<span
+					:class="[
+					$style.circle,
+					{[$style.circle]: step.completed === false},
+					{[$style.dot]: step.completed === true},
+					]"
+					@click="changeState(index)"
+				></span>
+			</div>
+			<p>{{ states[index].name }}</p>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  //   props: {
-  //     state: {
-  //       type: Array,
-  //       required: true
-  //     },
-  //     currentState: {
-  //       type: Object,
-  //       required: true
-  //     }
-  //   },
-  data() {
-    return {
-      states: [
-        { name: "Cart", completed: false },
-        { name: "Fulfillment", completed: false },
-        { name: "Payment", completed: false },
-        { name: "Review", completed: false }
-      ],
-      currentState: { name: "Cart", completed: false },
-      currentIndex: 0
-    };
-  },
-  computed: {
-    current() {
-      // Any time currentIndex is updated this will re-render.
-      // Use this to set current
-      currentIndex = this.states.indexOf(this.currentState);
-      return this.state[this.currentIndex];
-    }
-  },
-  methods: {
-    // Separation of responsibilities
-    changeState(index) {
-      // $emit a click event that returns the name of the state (e.g. 'Payment')
-      //   value.current = !value.current;
-      this.currentState = this.states[index];
-    }
-  }
+	props: {
+		states: {
+			type: Array,
+			required: true
+		},
+		currentState: {
+			type: Number,
+			required: true
+		}
+	},
+	data() {
+		return {
+			currentIndex: this.currentState
+		};
+	},
+	methods: {
+		changeState(index) {
+			if (this.states[index].completed || index === this.currentState) {
+				this.currentIndex = index;
+				this.$emit("newState", this.currentIndex);
+			}
+		}
+	}
 };
 </script>
 
-<style lang="scss" scoped>
-// Be mindful  that all of this needs to be mapped to Pressed engineering standards and color variables.
-.test {
-  background-color: #00ffff4d;
-}
+<style module lang="scss">
+@import "../styles/variables.scss";
 
 .box {
-  display: flex;
-  position: relative;
-  justify-content: space-around;
-  align-items: center;
+	display: flex;
+	position: relative;
+	justify-content: space-around;
+	align-items: center;
 }
 
 .line {
-  width: 100%;
-  height: 3px;
-  position: absolute;
-  border-radius: 1px;
-  z-index: -1;
-  top: 14px;
-  background-color: #bbb;
+	width: 100%;
+	height: $spacing-02;
+	position: absolute;
+	border-radius: $spacing-03;
+	z-index: -1;
+	top: $spacing-04;
+	background-color: $gray-30;
 }
 
 .dot {
-  height: 15px;
-  width: 15px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
+	height: $spacing-05;
+	width: $spacing-05;
+	background-color: $gray-30;
+	border-radius: 50%;
 }
 
 .circle {
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-  height: 15px;
-  width: 15px;
-  border: 4px solid #bbb;
-  border-radius: 50%;
-  display: inline-block;
+	box-sizing: border-box;
+	height: $spacing-05;
+	width: $spacing-05;
+	border: $spacing-02 solid $gray-30;
+	border-radius: 50%;
 }
 
 .current {
-  p {
-    color: black;
-  }
+	p {
+		color: $color-primary;
+	}
 
-  .dot {
-    background-color: black;
-  }
+	.dot {
+		background-color: $color-primary;
+	}
 
-  .circle {
-    border-color: black;
-  }
+	.circle {
+		border-color: $color-primary;
+	}
 }
 
-.bubble-box {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 30px;
-  width: 30px;
-  background-color: white;
+.bubbleBox {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: $spacing-07;
+	width: $spacing-07;
+	background-color: $white;
 }
 
-.step-cont {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  color: #bbb;
-  font-family: Verdana, Geneva, sans-serif;
+.stepContainer {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	color: $gray-30;
 }
 </style>
--- stories/components/basic/steps-progress-bar.js
+++ stories/components/basic/steps-progress-bar.js
@@ -1,29 +1,28 @@
 import StepProgressBar from '../../../components/step-progress-bar.vue';
 
-// You can store the data as a constant and reference that.
-
 export default {
-    component: StepProgressBar,
-    title: "Components / Basic / StepProgressBar"
+	component: StepProgressBar,
+	title: "Components / Basic / StepProgressBar",
 }
 
 export function Overview() {
-    return {
-        components: { StepProgressBar },
-        template: `
-            <step-progress-bar :states="states" :currentState="currentState" />
-         `,
-        data() {
-            return {
-                states: [
-                    { name: "Cart", completed: false },
-                    { name: "Fulfillment", completed: false },
-                    { name: "Payment", completed: false },
-                    { name: "Review", completed: false }
-                ],
-                currentState: { name: "Cart", completed: false }
-            }
-        }
-    }
+	return {
+		components: { StepProgressBar },
+		template: `
+			<step-progress-bar :states="states" :currentState="currentState" />
+		 `,
+		data() {
+			return {
+				states: [
+					{ name: "Cart", completed: true },
+					{ name: "Fulfillment", completed: true },
+					{ name: "Payment", completed: false },
+					{ name: "Review", completed: false }
+				],
+				currentState: 2,
+				currentIndex: this.currentState
+			}
+		}
+	}
 }
 
