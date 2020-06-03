03142610c8ba1e16b3a68648dde08d531d89fbaa
Spencer <slalexander92@gmail.com>
2020-06-03 08:18:06 -0700

    style(sidebar-overlay): change method of making overlay full-screen

--- components/sidebar-overlay.vue
+++ components/sidebar-overlay.vue
@@ -32,8 +32,10 @@ export default {
 
 	.overlay {
 		position: fixed;
-		height: 100vh;
-		width: 100%;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
 		z-index: 2;
 		background-color: rgba(0, 0, 0, 0.25);
 		opacity: 0;
