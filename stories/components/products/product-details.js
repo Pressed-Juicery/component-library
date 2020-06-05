fc7e7302d680d7c96bb4e5d34d07a5cc062657c9
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-05 10:31:27 -0700

    linting(product-details.js) added template strings

--- stories/components/products/product-details.js
+++ stories/components/products/product-details.js
@@ -12,22 +12,35 @@ export function Overview() {
 		data() {
 			return {
 				productInfo: {
-					overview: 'With 190% of your daily vitamin C, this blood orange juice is the perfect elevated OJ to take you from winter to spring!',
+					overview: `
+						With 190% of your daily vitamin C, this blood orange juice is the perfect
+						 elevated OJ to take you from winter to spring!
+					`,
 					details: [
 						{
 							icon: 'greens',
 							title: 'Your Daily Greens: Leveled Up',
-							content: 'The combination of apple cider vinegar with fresh-pressed juices from ginger, spinach & more add minerals and prebiotics.',
+							content: `
+								The combination of apple cider vinegar with fresh-pressed juices
+								 from ginger, spinach & more add minerals and prebiotics.
+							`,
 						},
 						{
 							icon: 'probiotics',
 							title: 'With the Mother',
-							content: 'Bragg apple cider vinegar contains the amazing vinegar “Mother,” which occurs naturally as it produces prebiotics.',
+							content: `
+								Bragg apple cider vinegar contains the amazing vinegar “Mother,” which
+								 occurs naturally as it produces prebiotics.
+							`,
 						},
 						{
 							icon: 'acv',
 							title: 'Balancing Your Appetite',
-							content: 'Bragg apple cider vinegar is full of zesty natural goodness. Studies show when incorporated as part of a healthy diet, it may help maintain a healthy weight - it’s associated with increased feelings of fullness!',
+							content: `
+								Bragg apple cider vinegar is full of zesty natural goodness. Studies show when
+								 incorporated as part of a healthy diet, it may help maintain a healthy weight - 
+								 it’s associated with increased feelings of fullness!
+							`,
 						},
 					],
 				},
