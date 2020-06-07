fa519642491cb73fd66dcf22ce0bacfbeb0aee9c
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-07 11:59:45 -0700

    layout(drawer) completed starting styles

--- components/drawer.vue
+++ components/drawer.vue
@@ -1,64 +1,114 @@
 <template>
-<div :class='$style.root'>
-	<div :class='$style.titleBar'>
-		<div :class='$style.title'>{{ title }}</div>
-		<ArrowDownIcon :class='$style.icon'/>
+	<div :class="$style.root">
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+		/>
+		<div>
+		<div v-if="isOpen" :class="$style.clickArea" @click="isOpen = !isOpen"></div>
+			<transition
+				name="custom-classes-transition"
+				enter-active-class="animate__animated animate__fadeInUp"
+				leave-active-class="animate__animated animate__fadeOutDown"
+			>
+				<div v-if="isOpen" :class="$style.drawer" key="drawer">
+					<div :class="$style.titleBar" @click="isOpen = !isOpen">
+						<div :class="$style.container">
+							<div :class="$style.title">{{ title }}</div>
+							<ArrowDownIcon :class="$style.icon" />
+						</div>
+					</div>
+					<div :class="$style.drawerContent">
+						<div :class="$style.container">
+							<slot />
+							<div style="width: 100%">
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<button style="width: 100%;" @click="isOpen = !isOpen">Done</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</transition>
+		</div>
+		<button style="position: fixed; top: 0;" @click="isOpen = !isOpen">Toggle Drawer</button>
 	</div>
-	<div :class='$style.drawerContent'>
-		<slot />
-	</div>
-</div>
 </template>
 
 <script>
-import ArrowDownIcon from './icons/arrow-down-icon.vue';
+import ArrowDownIcon from "./icons/arrow-down-icon.vue";
 export default {
 	props: {
 		title: {
 			type: String,
-			required: true,
+			required: true
 		},
 		isOpen: {
 			type: Boolean,
-			require: true,
-		},
+			require: true
+		}
 	},
-	components: { ArrowDownIcon }
+	components: { ArrowDownIcon },
+	methods: {}
 };
 </script>
 
 <style module lang="scss">
 @import "../styles/variables.scss";
 @import "../styles/mixins.scss";
-.root {
+
+@mixin flex($flow: row wrap, $justify: center, $align: center){
+	display: flex;
+	flex-flow: $flow;
+	justify-content: $justify;
+	align-items: $align;
+}
+
+.drawer {
+	z-index: 2;
 	position: fixed;
 	bottom: 0;
+	@include flex(column, center, space-around);
 	width: 100%;
-	height: 200px;
+}
+
+.clickArea {
+	position: relative;
+	top:0;
+	z-index: 1;
+	height: 100vh;
+	cursor: pointer;
 }
 
 .icon {
-	position: absolute;
-	right: 40px;
-	top: -45px;
-	width: 15px;
-	height: 15px;
+	width: $spacing-03;
+	height: $spacing-03;
 }
 
 .titleBar {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin-top: -75px;
-	height: 75px;
-	border-radius: 30px 30px 0 0;
+	@include flex(row, space-around, center);
+	height: $spacing-10;
+	background-color: white;
+	z-index: 1;
+	border-radius: $spacing-05 $spacing-05 0 0;
 	box-shadow: 0px -2px 7px 1px $gray-20;
+	cursor: pointer;
 }
 
 .drawerContent {
-	display: flex;
-	justify-content: center;
-	align-items: center;
+	@include flex();
+	padding: $spacing-07 0;
+	background-color: white;
+}
+
+.container {
+	width: 75%;
+	@include flex(row, space-between, center);
 }
 
 .title {
