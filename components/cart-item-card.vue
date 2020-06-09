58aa5e531a2c2233af9099d99de79375b48fd58d
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-08 19:42:03 -0700

    feat(cart item card): update cart item properties

--- components/cart-item-card.vue
+++ components/cart-item-card.vue
@@ -1,18 +1,26 @@
 <template>
 	<card :class="$style.root">
-		<div :class="$style.imageWrapper">
-			<img :class="$style.image" :src="item.imageSrc" :alt="item.name" />
-		</div>
-
-		<div :class="$style.descriptionWrapper">
-			<div :class="$style.description">
-				<div :class="$style.title">{{ item.name }}</div>
-				<div :class="$style.additionalInfo">{{ additionalInformation }}</div>
-				<div :class="$style.quantity">Qty {{ item.quantity }}</div>
+		<div :class="$style.mainContent">
+			<div :class="$style.imageWrapper">
+				<img :class="$style.image" :src="item.variant.imageUrl" :alt="item.variant.name" />
 			</div>
 
-			<span :class="$style.price">{{ displayPrice }}</span>
+			<div :class="$style.descriptionWrapper">
+				<div :class="$style.description">
+					<div :class="$style.title">{{ item.variant.name }}</div>
+					<div :class="$style.quantity">Qty {{ item.quantity }}</div>
+				</div>
+
+				<div>
+					<span :class="$style.originalPrice">{{ originalPrice }}</span>
+					<span :class="$style.price">{{ displayPrice }}</span>
+				</div>
+			</div>
 		</div>
+
+		<hr v-if="additionalContent" :class="$style.divider" />
+
+		<div v-if="additionalContent" :class="$style.additionalContent">{{ additionalContent }}</div>
 	</card>
 </template>
 
@@ -28,7 +36,7 @@ export default {
 	},
 
 	computed: {
-		additionalInformation() {
+		additionalContent() {
 			return this.item.additionalInformation
 				|| this.item.bundleItems
 				|| (this.item.modifiers && this.item.modifiers.toppings)
