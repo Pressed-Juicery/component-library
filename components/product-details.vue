95a401aedbca8b914246e1b29b4a6230afc9f40e
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-06-09 23:44:34 -0700

    style(product-detail): indent template and styles

--- components/product-details.vue
+++ components/product-details.vue
@@ -1,16 +1,16 @@
 <template>
-<div>
-	<div :class="$style.overview">{{ productInfo.overview }}</div>
-	<div :class="$style.row" v-for="(detail, index) in productInfo.details" :key="index">
-		<div :class="$style.iconWrapper">
-			<img :class="$style.icon" :src="detail.iconUrl" alt />
-		</div>
-		<div>
-			<div :class="$style.title">{{ detail.title }}</div>
-			<div>{{ detail.content }}</div>
+	<div>
+		<div :class="$style.overview">{{ productInfo.overview }}</div>
+		<div :class="$style.row" v-for="(detail, index) in productInfo.details" :key="index">
+			<div :class="$style.iconWrapper">
+				<img :class="$style.icon" :src="detail.iconUrl" alt />
+			</div>
+			<div>
+				<div :class="$style.title">{{ detail.title }}</div>
+				<div>{{ detail.content }}</div>
+			</div>
 		</div>
 	</div>
-</div>
 </template>
 
 <script>
@@ -25,31 +25,31 @@ export default {
 </script>
 
 <style module lang="scss">
-@import "../styles/variables.scss";
-@import "../styles/mixins.scss";
+	@import "../styles/variables.scss";
+	@import "../styles/mixins.scss";
 
-.overview {
-	margin-bottom: $spacing-07;
-}
+	.overview {
+		margin-bottom: $spacing-07;
+	}
 
-.row {
-	display: flex;
-	margin-bottom: $spacing-07;
-}
+	.row {
+		display: flex;
+		margin-bottom: $spacing-07;
+	}
 
-.iconWrapper {
-	flex-shrink: 0;
-	text-align: center;
-	width: $spacing-10;
-	margin-right: $spacing-05;
-}
+	.iconWrapper {
+		flex-shrink: 0;
+		text-align: center;
+		width: $spacing-10;
+		margin-right: $spacing-05;
+	}
 
-.icon {
-	height: $spacing-08;
-}
+	.icon {
+		height: $spacing-08;
+	}
 
-.title {
-	@include text-bolder();
-	margin-bottom: $spacing-02;
-}
+	.title {
+		@include text-bolder();
+		margin-bottom: $spacing-02;
+	}
 </style>
