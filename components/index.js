f3bf0b0eb6515550c96be51e0e838b41797cc234
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-09 20:17:29 -0700

    linting(product-details) ci linting error

--- components/index.js
+++ components/index.js
@@ -21,7 +21,7 @@ export { default as MediaRow } from './media-row';
 export { default as PaymentMethodIcon } from './payment-method-icon';
 export { default as PaymentMethodRadioButtonCard } from './payment-method-radio-button-card';
 export { default as PointsRedemption } from './points-redemption';
-export { default as ProductDetails } from './product-details.vue'
+export { default as ProductDetails } from './product-details.vue';
 export { default as QuantitySelector } from './quantity-selector';
 export { default as RadioButton } from './radio-button';
 export { default as RadioButtonCard } from './radio-button-card';
