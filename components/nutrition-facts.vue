dc0794abcd644144132ed6943ab5a8cb09540b43
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-06-09 17:02:32 -0700

    fix(nutrition-facts): fix grammar

--- components/nutrition-facts.vue
+++ components/nutrition-facts.vue
@@ -29,7 +29,7 @@
 				</span>
 			</div>
 			<div :class="$style.dailyValue">
-				The % Daily Value tells you how much nutrient in a serving of
+				The % Daily Value tells you how much a nutrient in a serving of
 				food contributes to a daily diet. 2,000 calories a day
 				is used for general nutritional advice.
 			</div>
