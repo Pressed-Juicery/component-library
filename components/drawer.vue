d3e19670454fefab88fedad899f3e3c2c2e6ecd0
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-09 23:16:35 -0700

    refactor(drawer) cleaning up code

--- components/drawer.vue
+++ components/drawer.vue
@@ -1,38 +1,42 @@
 <template>
-	<div :class='$style.root'>
+	<div>
 		<link
-			rel='stylesheet'
-			href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css'
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
 		/>
 		<div>
-		<div v-if='isOpen' :class='$style.clickArea' @click='isOpen = !isOpen'></div>
+		<div v-if="isOpen" :class="$style.clickArea" @click="toggleOpen"></div>
 			<transition
-				name='custom-classes-transition'
-				enter-active-class='animate__animated animate__fadeInUp'
-				leave-active-class='animate__animated animate__fadeOutDown'
+				name="custom-classes-transition"
+				enter-active-class="animate__animated animate__fadeInUp animate__faster"
+				leave-active-class="animate__animated animate__fadeOutDown animate__faster"
 			>
-				<div v-if='isOpen' :class='$style.drawer' key='drawer'>
-					<div :class='$style.titleBar' @click='isOpen = !isOpen'>
-						<div :class='$style.container'>
-							<div :class='$style.title'>{{ title }}</div>
-							<ArrowDown :class='$style.icon' />
+				<div v-if="isOpen" :class="$style.drawer" key="drawer">
+					<div :class="$style.titleBar" @click="toggleOpen">
+						<div :class="$style.container">
+							<div :class="$style.title">{{ title }}</div>
+							<ArrowDown :class="$style.icon" />
 						</div>
 					</div>
-					<div :class='$style.drawerContent'>
-						<div :class='$style.container'>
+					<div :class="$style.drawerContent">
+						<div :class="$style.container">
 							<slot />
-							<button style='width: 100%;' @click='isOpen = !isOpen'>Done</button>
+							<div>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+								<h3>Test</h3>
+							</div>
 						</div>
 					</div>
 				</div>
 			</transition>
 		</div>
-		<button style='position: fixed; top: 0;' @click='isOpen = !isOpen'>Toggle Drawer</button>
 	</div>
 </template>
 
 <script>
-import ArrowDown from './icons/arrow-down-icon.vue';
+import ArrowDown from "./icons/arrow-down-icon.vue";
 export default {
 	props: {
 		title: {
@@ -45,26 +49,31 @@ export default {
 		},
 	},
 	components: { ArrowDown },
+	methods: {
+		toggleOpen() {
+			this.$emit("toggleOpen", !this.isOpen);
+		}
+	},
 };
 </script>
 
-<style module lang='scss'>
-@import '../styles/variables.scss';
-@import '../styles/mixins.scss';
+<style module lang="scss">
+@import "../styles/variables.scss";
+@import "../styles/mixins.scss";
 
-@mixin flex($flow: row wrap, $justify: center, $align: center){
+.drawer, .drawerContent {
 	display: flex;
-	flex-flow: $flow;
-	justify-content: $justify;
-	align-items: $align;
+	justify-content: center;
 }
 
 .drawer {
 	z-index: 2;
 	position: fixed;
-	@include flex(column, center, space-around);
+	flex-flow: column;
+	align-items: space-around;
 	bottom: 0;
 	width: 100%;
+	background-color: $red-20;
 }
 
 .clickArea, .titleBar {
@@ -74,17 +83,18 @@ export default {
 .clickArea {
 	z-index: 1;
 	position: relative;
-	top:0;
 	height: 100vh;
+	background-color: $gray-90;
+	opacity: 0.3;
 }
 
 .icon {
 	width: $spacing-03;
-	height: $spacing-03;
 }
 
 .titleBar {
-	@include flex(row, space-around, center);
+	display: flex;
+	justify-content: space-around;
 	z-index: 1;
 	height: $spacing-10;
 	border-radius: $spacing-05 $spacing-05 0 0;
@@ -93,14 +103,16 @@ export default {
 }
 
 .drawerContent {
-	@include flex();
+	align-items: center;
 	padding: $spacing-07 0;
 	background-color: $white;
 }
 
 .container {
 	width: 75%;
-	@include flex(row, space-between, center);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 
 .title {
--- stories/components/basic/drawer.js
+++ stories/components/basic/drawer.js
@@ -5,7 +5,7 @@ export default {
 	component: Drawer,
 };
 
-export function Overview() {
+export function DrawerOpen() {
 	return {
 		components: { Drawer },
 		template: '<drawer :title="title" :isOpen="isOpen"></drawer>',
@@ -13,6 +13,18 @@ export function Overview() {
 			return {
 				title: 'enhance your cleanse',
 				isOpen: true,
+			};
+		},
+	};
+}
+export function DrawerClosed() {
+	return {
+		components: { Drawer },
+		template: '<drawer :title="title" :isOpen="isOpen"></drawer>',
+		data() {
+			return {
+				title: 'enhance your cleanse',
+				isOpen: false,
 			};
 		},
 	};
