b4f25205fd9474d3919cad54919cc2c29a37531b
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-06-09 18:08:11 -0700

    style(nutrition-facts): adjust line breaks

--- components/nutrition-facts.vue
+++ components/nutrition-facts.vue
@@ -25,9 +25,8 @@
 		</div>
 
 		<div :class="$style.dailyValue">
-			The % Daily Value tells you how much a nutrient in a serving of
-			food contributes to a daily diet. 2,000 calories a day
-			is used for general nutritional advice.
+			The % Daily Value tells you how much a nutrient in a serving of food contributes to a daily diet.
+			2,000 calories a day is used for general nutritional advice.
 		</div>
 
 		<div :class="$style.claims">
