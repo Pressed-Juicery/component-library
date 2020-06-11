f7a11e066ef148212bce8c918ff044a6fe0b2008
Spencer <slalexander92@gmail.com>
2020-06-11 09:49:04 -0700

    feat(cart-points-display): pass in user object instead of just points

--- components/cart-points-display.vue
+++ components/cart-points-display.vue
@@ -1,23 +1,30 @@
 <template>
-  <div>
+  <div v-if="user && user.points >= 0">
     <div :class="$style.balanceWrapper">
       <div>Pressed Points Balance</div>
-      <div>{{ points }}</div>
+      <div>{{ user.points }}</div>
     </div>
     <button @click="$emit('stateChange', 'points')" :class="$style.redeemPoints">Redeem Pressed Points</button>
+		<hr :class="$style.partition">
   </div>
 </template>
 
 <script>
 export default {
 	props: {
-		points: Number,
+		user: Object,
 	},
 };
 </script>
 
 <style module lang="scss">
   @import '../styles/variables';
+
+	.partition {
+		border: 1px solid $gray-30;
+		margin-top: $spacing-05;
+		margin-bottom: $spacing-06;
+	}
 
 	.balanceWrapper {
 		display: flex;
