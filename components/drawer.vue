eab2b85779254db81fa691e63867db1e9456fdd6
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-10 19:32:11 -0700

    style(drawer) restructuring logical flow

--- components/drawer.vue
+++ components/drawer.vue
@@ -1,32 +1,26 @@
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+/>
 <template>
 	<div>
-		<link
-			rel="stylesheet"
-			href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
-		/>
 		<div>
-		<div v-if="isOpen" :class="$style.clickArea" @click="toggleOpen"></div>
+			<div v-if="isOpen" :class="$style.overlay" @click="toggleDrawer"></div>
 			<transition
 				name="custom-classes-transition"
-				enter-active-class="animate__animated animate__fadeInUp animate__faster"
-				leave-active-class="animate__animated animate__fadeOutDown animate__faster"
+				enter-to-class="animate__animated animate__fadeInUp"
+				leave-to-class="animate__animated animate__fadeOutDown"
 			>
-				<div v-if="isOpen" :class="$style.drawer" key="drawer">
-					<div :class="$style.titleBar" @click="toggleOpen">
-						<div :class="$style.container">
+				<div v-if="isOpen" :class="$style.drawer">
+					<div :class="$style.drawerHeader" @click="toggleDrawer">
+						<div :class="$style.wrapper">
 							<div :class="$style.title">{{ title }}</div>
 							<ArrowDown :class="$style.icon" />
 						</div>
 					</div>
 					<div :class="$style.drawerContent">
-						<div :class="$style.container">
+						<div :class="$style.wrapper">
 							<slot />
-							<div>
-								<h3>Test</h3>
-								<h3>Test</h3>
-								<h3>Test</h3>
-								<h3>Test</h3>
-							</div>
 						</div>
 					</div>
 				</div>
@@ -41,19 +35,19 @@ export default {
 	props: {
 		title: {
 			type: String,
-			required: true,
+			required: true
 		},
 		isOpen: {
 			type: Boolean,
-			require: true,
-		},
+			require: true
+		}
 	},
 	components: { ArrowDown },
 	methods: {
-		toggleOpen() {
-			this.$emit("toggleOpen", !this.isOpen);
+		toggleDrawer() {
+			this.$emit("toggleDrawer", !this.isOpen);
 		}
-	},
+	}
 };
 </script>
 
@@ -61,26 +55,7 @@ export default {
 @import "../styles/variables.scss";
 @import "../styles/mixins.scss";
 
-.drawer, .drawerContent {
-	display: flex;
-	justify-content: center;
-}
-
-.drawer {
-	z-index: 2;
-	position: fixed;
-	flex-flow: column;
-	align-items: space-around;
-	bottom: 0;
-	width: 100%;
-	background-color: $red-20;
-}
-
-.clickArea, .titleBar {
-	cursor: pointer;
-}
-
-.clickArea {
+.overlay {
 	z-index: 1;
 	position: relative;
 	height: 100vh;
@@ -88,34 +63,67 @@ export default {
 	opacity: 0.3;
 }
 
-.icon {
-	width: $spacing-03;
+.drawer {
+	position: fixed;
+	flex-flow: column;
+	bottom: 0;
+	width: 100%;
 }
 
-.titleBar {
+.drawer,
+.drawerContent,
+.drawerHeader,
+.wrapper {
 	display: flex;
+}
+
+.drawer,
+.drawerContent {
+	justify-content: center;
+}
+
+.drawerContent,
+.wrapper {
+	align-items: center;
+}
+
+.drawer,
+.drawerHeader {
+	z-index: 2;
+}
+
+.drawerHeader,
+.drawerContent {
+	background-color: $white;
+}
+
+.overlay,
+.drawerHeader {
+	cursor: pointer;
+}
+
+.wrapper {
+	width: 75%;
+	justify-content: space-between;
+
+}
+
+.drawerHeader {
 	justify-content: space-around;
-	z-index: 1;
 	height: $spacing-10;
 	border-radius: $spacing-05 $spacing-05 0 0;
 	box-shadow: 0px -1px 5px $gray-20;
-	background-color: $white;
-}
-
-.drawerContent {
-	align-items: center;
-	padding: $spacing-07 0;
-	background-color: $white;
-}
-
-.container {
-	width: 75%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
 }
 
 .title {
 	@include text-cta();
+}
+
+.icon {
+	width: $spacing-03;
+}
+
+.drawerContent {
+	padding: $spacing-07 0;
 }
 </style>
