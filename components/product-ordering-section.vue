d692806f36de75eb6dca9f60b4e633211e8544f5
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-12 21:51:03 -0700

    initial(product-ordering-section) first pass

--- components/product-ordering-section.vue
+++ components/product-ordering-section.vue
@@ -1,21 +1,27 @@
 <template>
 	<div>
 		<div :class="$style.productName">{{ product.name }}</div>
+		<div :class="$style.images"></div>
 		<div :class="$style.information">
 			<div
 				:class="$style.price"
-			>${{ isVip ? product.variants[currentVariant].memberPrice : product.variants[currentVariant].nonMemberPrice }}</div>
+			>${{ isVip 
+				? product.variants[currentVariant].memberPrice 
+				: product.variants[currentVariant].nonMemberPrice 
+			}}</div>
 			<div :class="$style.calories">{{ product.variants[currentVariant].calories }} cal</div>
 		</div>
-		<select v-model="currentVariant">
+		<select :class="$style.variant" v-model="currentVariant">
 			<option
 				v-for="(variant, index) in product.variants"
 				:value="index"
 				:key="index"
 			>{{ variant.name }}</option>
 		</select>
-		<input type="number" v-model="quantity" min="0" oninput="validity.valid||(value='');" />
-		<button :disabled="isDisabled" @click="addedToCart">add to cart</button>
+		<div :class="$style.actionGroup">
+			<input :class="$style.quantity" type="number" v-model="quantity" min="0" oninput="validity.valid||(value='');" />
+			<button :class="$style.addToCartButton" :disabled="isDisabled" @click="addedToCart">add to cart</button>
+		</div>
 	</div>
 </template>
 
@@ -29,7 +35,7 @@ export default {
 		product: {
 			type: Object,
 			required: true
-		}
+		},
 	},
 	data() {
 		return {
@@ -40,17 +46,17 @@ export default {
 	computed: {
 		isDisabled() {
 			return this.quantity < 1 ? true : false;
-		}
+		},
 	},
 	methods: {
 		addedToCart() {
 			let cartItem = {
 				variantId: this.product.variants[this.currentVariant].id,
-				quantity: Number(this.quantity)
+				quantity: Number(this.quantity),
 			};
 			this.$emit("addedToCart", cartItem);
-		}
-	}
+		},
+	},
 };
 </script>
 
@@ -60,17 +66,56 @@ export default {
 
 .productName {
 	@include text-heading-4();
+	margin-bottom: $spacing-03;
 }
 
 .information {
 	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: $spacing-06;
 }
 
-.information {
-	justify-content: space-between;
+.images {
+	height: 344px;
+	margin-bottom: $spacing-07;
 }
 
 .price {
 	@include text-heading-5();
 }
+
+.actionGroup {
+	display: grid;
+	grid-template-columns: 88px 1fr;
+	grid-column-gap: $spacing-03;
+}
+
+.variant {
+	text-indent: $spacing-06;
+    background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_up.png) no-repeat right;
+	background-position-x: calc(100% - 24px);
+}
+
+.variant,
+.quantity,
+.addToCartButton {
+	height: $spacing-09;
+}
+
+.variant,
+.quantity {
+	background-color: #ffffff;
+	border-radius: $spacing-04;
+	border-color: transparent;
+	margin-bottom: $spacing-03;
+}
+
+.quantity,
+.quantity::-webkit-inner-spin-button,
+.quantity::-webkit-outer-spin-button {
+	-webkit-appearance: none;
+	text-align: center;
+}
+
 </style>
