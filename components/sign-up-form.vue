9d8d36aa737a41d1b38cc49a877c66da8fc9ed0b
Spencer <slalexander92@gmail.com>
2020-06-16 07:54:07 -0700

    feat(sign-up-form): import components needed

--- components/sign-up-form.vue
+++ components/sign-up-form.vue
@@ -3,8 +3,12 @@
 </template>
 
 <script>
-export default {
+import RadioButtonCard from './radio-button-card';
+import ValidatedForm from './validated-form';
+import ValidatedInput from './validated-input';
 
+export default {
+  components: { RadioButtonCard, ValidatedForm, ValidatedInput },
 }
 </script>
 
