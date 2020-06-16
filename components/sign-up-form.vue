48063fa0eb5a8c8e7a0b46f93e0e083a69d3bdf2
Spencer <slalexander92@gmail.com>
2020-06-16 09:46:49 -0700

    feat(sign-up-form): add v-model, add rules property placeholder, import rule utilities

--- components/sign-up-form.vue
+++ components/sign-up-form.vue
@@ -1,13 +1,21 @@
 <template>
 	<validated-form>
 		<div :class="$style.row">
-			<validated-input :class="$style.inputHalf" label="First Name"/>
-			<validated-input :class="$style.inputHalf" label="Last Name"/>
+			<validated-input
+				:class="$style.inputHalf"
+				:rules="rules.firstName"
+				v-model="user.firstName"
+				label="First Name"/>
+			<validated-input
+				:class="$style.inputHalf"
+				:rules="rules.lastName"
+				v-model="user.lastName"
+				label="Last Name"/>
 		</div>
-			<validated-input label="Email"/>
-			<validated-input label="Password"/>
-			<validated-input label="Birthday (optional)"/>
-			<validated-input label="Phone Number"/>
+		<validated-input :rules="rules.email" v-model="user.email" label="Email"/>
+		<validated-input :rules="rules.password" v-model="user.password" label="Password"/>
+		<validated-input :rules="rules.birthday" v-model="user.birthday" label="Birthday (optional)"/>
+		<validated-input :rules="rules.phone" v-model="user.phone" label="Phone Number"/>
 		<div>
 			<radio-button-card value="points" :selectedValue="selectedCard" @change="handleCardSelect">
 				<div :class="$style.cardTitle">Just Points & Rewards</div>
@@ -32,6 +40,7 @@
 </template>
 
 <script>
+import { hasExactLength, isNotEmpty, isNumber, isValidPhoneNumber } from '../utilities/validators';
 import RadioButtonCard from './radio-button-card';
 import ValidatedForm from './validated-form';
 import ValidatedInput from './validated-input';
@@ -46,6 +55,40 @@ export default {
 	data() {
 		return {
 			selectedCard: '',
+			user: {
+				firstName: '',
+				lastName: '',
+				email: '',
+				password: '',
+				birthday: '',
+				phone: '',
+			},
+			rules: {
+				firstName: [{
+					validator: isNotEmpty,
+					message: 'Please enter your first name.',
+				}],
+				lastName: [{
+					validator: isNotEmpty,
+					message: 'Please enter your first name.',
+				}],
+				email: [{
+					validator: isNotEmpty,
+					message: 'Please enter your first name.',
+				}],
+				password: [{
+					validator: isNotEmpty,
+					message: 'Please enter your first name.',
+				}],
+				birthday: [{
+					validator: isNotEmpty,
+					message: 'Please enter your first name.',
+				}],
+				phone: [{
+					validator: isNotEmpty,
+					message: 'Please enter your first name.',
+				}],
+			},
 		};
 	},
 
