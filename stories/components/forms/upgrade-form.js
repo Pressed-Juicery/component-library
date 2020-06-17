6d1c09d742c8c8342eccb852b2e9f21dd2f7c7ff
Spencer <slalexander92@gmail.com>
2020-06-17 13:39:34 -0700

    feat(upgrade-form): add story for upgrade form

--- stories/components/forms/upgrade-form.js
+++ stories/components/forms/upgrade-form.js
@@ -8,6 +8,37 @@ export default {
 export function Overview() {
 	return {
 		components: { UpgradeForm },
-		template: '<upgrade-form />',
+		template: `
+			<div>
+				<upgrade-form :selectedAmount="selectedAmount" :reloadAmounts="reloadAmounts" @change="handlePaymentMethodChange">
+					<button type="submit">submit</button>
+				</upgrade-form>
+				<p>data:
+				<code>{{ data }}</code>
+				</p>
+			</div>
+		`,
+		data() {
+			return {
+				selectedAmount: null,
+				data: null,
+			};
+		},
+		methods: {
+			handlePaymentMethodChange(data) {
+				this.data = data;
+			}
+		},
+		computed: {
+			reloadAmounts(){
+				return Array.from(Array(10), (_, i) => {
+					const reloadAmount = (i + 1) * 10;
+					return {
+						name: reloadAmount,
+						value: reloadAmount,
+					};
+				});
+			},
+		},
 	};
 }
