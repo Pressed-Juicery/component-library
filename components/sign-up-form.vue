1c6be97056aa20dc328c324197dc542eb20fd945
Spencer <slalexander92@gmail.com>
2020-06-17 13:26:21 -0700

    feat(sign-up-form): make sign-up-form component just the sign-up form

--- components/sign-up-form.vue
+++ components/sign-up-form.vue
@@ -1,5 +1,5 @@
 <template>
-	<validated-form @submit="handleFormSubmit">
+	<validated-form ref="signup" @submit="$emit('submit', user)">
 		<div :class="$style.row">
 			<validated-input
 				:class="$style.inputHalf"
@@ -13,51 +13,26 @@
 				label="Last Name"/>
 		</div>
 		<validated-input :rules="rules.email" v-model="user.email" label="Email"/>
-		<validated-input :rules="rules.password" v-model="user.password" label="Password"/>
-		<validated-input :rules="rules.birthday" v-model="user.birthday" label="Birthday (optional)"/>
+		<validated-input type="password" :rules="rules.password" v-model="user.password" label="Password"/>
+		<validated-input type="date" :rules="rules.birthday" v-model="user.birthday" label="Birthday (optional)"/>
 		<validated-input :rules="rules.phone" v-model="user.phone" label="Phone Number"/>
-		<div>
-			<radio-button-card value="points" :selectedValue="selectedCard" @change="handleCardSelect">
-				<div :class="$style.cardTitle">Just Points & Rewards</div>
-				<div :class="$style.cardBodyText">
-					As a Pressed Points Insider, you collect points on every order to be able to redeem for free items.
-				</div>
-			</radio-button-card>
-			<radio-button-card value="vip" :selectedValue="selectedCard" @change="handleCardSelect">
-				<div :class="$style.cardTitle">Get Discounts!</div>
-				<div :class="$style.cardBodyText">
-					Become a VIP member by committing to a minimum $10 monthly spend and
-					get discounted pricing everyday!
-				</div>
-				<upgrade-form v-if="selectedCard === 'vip'" :class="$style.upgradeForm"/>
-			</radio-button-card>
-		</div>
-		<div :class="$style.disclaimer">
-			By signing up you are agreeing to the <a :class="$style.disclaimerLink" href="#">Terms & Conditions</a>
-			and the <a :class="$style.disclaimerLink" href="#">Membership Program</a>.
-		</div>
-		<button :class="$style.submit">Confirm</button>
+		<slot />
 	</validated-form>
 </template>
 
 <script>
-import { isNotEmpty, isEmail, isValidPhoneNumber, hasMinimumLength } from '../utilities/validators';
-import RadioButtonCard from './radio-button-card';
-import UpgradeForm from './upgrade-form';
+import { isNotEmpty, isEmail, isValidPhoneNumber } from '../utilities/validators';
 import ValidatedForm from './validated-form';
 import ValidatedInput from './validated-input';
 
 export default {
 	components: {
-		RadioButtonCard,
-		UpgradeForm,
 		ValidatedForm,
 		ValidatedInput,
 	},
 
 	data() {
 		return {
-			selectedCard: '',
 			user: {
 				firstName: '',
 				lastName: '',
@@ -66,6 +41,7 @@ export default {
 				birthday: '',
 				phone: '',
 			},
+
 			rules: {
 				firstName: [{
 					validator: isNotEmpty,
@@ -96,16 +72,6 @@ export default {
 			},
 		};
 	},
-
-	methods: {
-		handleCardSelect(val) {
-			this.selectedCard = val;
-		},
-
-		handleFormSubmit() {
-
-		},
-	},
 };
 </script>
 
@@ -120,30 +86,12 @@ export default {
 	flex: 1 1 0;
 }
 
-.cardTitle {
-	@include text-body;
+.inputHalf:first-of-type {
+	margin-right: $spacing-05;
 }
 
-.cardBodyText {
-	@include text-body-small;
-}
-
-.disclaimer {
-	@include text-body-small;
-	text-align: center;
-	margin-bottom: $spacing-06;
-}
-
-.disclaimerLink {
-	text-decoration: underline;
-}
-
-.submit {
-	width: 100%;
-}
-
-.upgradeForm {
-	margin-top: $spacing-06;
+.inputHalf:nth-of-type(2) {
+	margin-left: $spacing-05;
 }
 
 </style>
