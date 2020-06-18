61025ed5927577d753f86a8ebd2fa6a1857a3904
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-17 21:55:07 -0700

    initial(cleanse-guide) first-pass solution

---
+++ components/cleanse-guide.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<div :class="$style.sectionTitle">Cleanse Guide</div>
+
+		<div :class="$style.cleanse" v-for="(cleanse, index) in cleanseGuide" :key="index">
+
+			<div :class="$style.title">{{ cleanse.timeOfDay }} | {{ cleanse.juice }}</div>
+			<div :class="$style.content">{{ cleanse.description }}</div>
+			<a :class="$style.learnMore" :href="cleanse.learnMoreUrl">Learn More</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		cleanseGuide: {
+			type: Array,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style module lang="scss">
+@import "../styles/mixins.scss";
+@import "../styles/variables.scss";
+
+.sectionTitle {
+	@include text-cta-small();
+	margin-bottom: $spacing-07;
+}
+
+.cleanse {
+	margin-bottom: $spacing-06;
+}
+
+.title {
+	@include text-cta-small();
+	margin-bottom: $spacing-03;
+}
+
+.content {
+	margin-bottom: $spacing-02;
+}
+
+.learnMore {
+	@include text-body-small();
+	border-bottom: 1px solid black;
+}
+</style>
---
+++ stories/components/products/cleanse-guide.js
@@ -0,0 +1,55 @@
+import CleanseGuide from '../../../components/cleanse-guide.vue';
+
+export default {
+	title: 'Components / Products / CleanseGuide',
+	component: CleanseGuide,
+};
+
+export function Overview() {
+	return {
+		components: { CleanseGuide },
+		template: '<cleanse-guide :cleanseGuide="cleanseGuide" />',
+		data() { // eslint-disable-line max-lines-per-function
+			return {
+				cleanseGuide: [
+					{
+					  timeOfDay: "morning",
+					  juice: "greens 2",
+					  description: "Start your day with a green juice to energize your body.",
+					  learnMoreUrl: "https://pressedjuicery.com/products/kale-spinach-juice",
+					},
+					{
+					  timeOfDay: "mid morning",
+					  juice: "citrus 2",
+					  description: "Great for digestion due to pineapple’s bromelain, Citrus 2 is also full of antioxidants & aids in detoxification.",
+					  learnMoreUrl: "https://pressedjuicery.com/products/kale-spinach-juice",
+					},
+					{
+					  timeOfDay: "noon",
+					  juice: "greens 3",
+					  description: "Get all the goodness of leafy greens, plus ginger to aid in digestion.",
+					  learnMoreUrl: "https://pressedjuicery.com/products/kale-spinach-juice",
+					},
+					{
+					  timeOfDay: "afternoon",
+					  juice: "roots 3",
+					  description: "Get all the goodness of leafy greens, plus ginger to aid in digestion.",
+					  learnMoreUrl: "https://pressedjuicery.com/products/kale-spinach-juice",
+					},
+					{
+					  timeOfDay: "late afternoon",
+					  juice: "citrus 1",
+					  description: "A soothing & very hydrating juice to have before your Vanilla Almond.",
+					  learnMoreUrl: "https://pressedjuicery.com/products/kale-spinach-juice",
+					},
+					{
+					  timeOfDay: "night",
+					  juice: "vanilla almond",
+					  description: "End your day sweet & satisfied with your protein & healthy fats.",
+					  learnMoreUrl: "https://pressedjuicery.com/products/kale-spinach-juice",
+					},
+				],
+			};
+		},
+	};
+}
