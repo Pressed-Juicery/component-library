ddf30a31c900b6233f35d3bb4afd2b416aa29800
Vinni Hoke <vinni.hoke@gmail.com>
2020-06-18 18:57:08 -0700

    initial(cleanse-bundle-includes) setup vinni/cleanse-bundle-includes

---
+++ components/cleanse-bundle-includes.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div :class="$style.sectionTitle"></div>
+		<div :class="$style.products">
+			<div :class="$style.product" v-for="(product, index) in cleanseBundle" :key="index">
+				<img :class="$style.productImage" src="" alt="">
+				<div :class="$style.productTitle"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			cleanseBundle: {
+				type: Array,
+				required: true,
+			},
+		},
+	};
+</script>
+
+<style module lang="scss">
+
+</style>
---
+++ stories/components/products/cleanse-bundle-includes.js
@@ -0,0 +1,25 @@
+import CleanseBundleIncludes from '../../../components/cleanse-bundle-includes.vue';
+
+export default {
+	title: 'Components / Products / CleanseBundleIncludes',
+	component: CleanseBundleIncludes
+}
+
+export function Overview() {
+	return {
+		components: { CleanseBundleIncludes },
+		template: '<cleanse-bundle-includes :cleanseBundle="cleanseBundle"/>',
+		data() {
+			return {
+				cleanseBundle: [
+					{ title: "Greens 1", imageUrl: "https://d3e4ixgvd0ibii.cloudfront.net/shopify/cleanse-pdp/GRN1.png" },
+					{ title: "Citrus 1", imageUrl: "https://d3e4ixgvd0ibii.cloudfront.net/shopify/cleanse-pdp/CIT1.png" },
+					{ title: "Greens 2", imageUrl: "https://d3e4ixgvd0ibii.cloudfront.net/shopify/cleanse-pdp/GRN1.png" },
+					{ title: "Citrus 2", imageUrl: "https://d3e4ixgvd0ibii.cloudfront.net/shopify/cleanse-pdp/CIT1.png" },
+					{ title: "Greens 3", imageUrl: "https://d3e4ixgvd0ibii.cloudfront.net/shopify/cleanse-pdp/GRN1.png" },
+					{ title: "Citrus 3", imageUrl: "https://d3e4ixgvd0ibii.cloudfront.net/shopify/cleanse-pdp/CIT1.png" },
+				]
+			}
+		}
+	}
+}
