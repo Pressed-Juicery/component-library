9db73dac8b126f457d91d3b1f37deada7f77c22d
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-22 14:52:29 -0700

    feat(checkout authentication stories): add checkout authentication stories

---
+++ stories/components/basic/checkout-authentication.js
@@ -0,0 +1,49 @@
+import { CHANGE, boolean, withKnobs } from '@storybook/addon-knobs';
+
+import CartSidebar from '../../../components/cart-sidebar';
+import CheckoutAuthentication from '../../../components/checkout-authentication';
+import Drawer from '../../../components/drawer';
+
+import { addons } from '@storybook/addons';
+
+export default {
+	title: 'Components / Basic / CheckoutAuthentication',
+	component: CheckoutAuthentication,
+	decorators: [withKnobs],
+};
+
+const mixins = [{
+	methods: {
+		closeDrawer() {
+			addons.getChannel().emit(CHANGE, {
+				name: 'DrawerOpen',
+				value: false,
+			});
+		},
+
+		closeSidebar() {
+			addons.getChannel().emit(CHANGE, {
+				name: 'SidebarOpen',
+				value: false,
+			});
+		},
+	},
+}];
+
+export function Overview() {
+	return {
+		components: { CheckoutAuthentication, Drawer },
+		mixins,
+
+		template: `
+			<div>
+				<checkout-authentication @sign-in="credentials => user = credentials"
+				                         @add-guest="guestInfo => guest = guestInfo" />
+				<div v-if="user">User: {{ user }}</div>
+				<div v-if="guest">Guest: {{ guest }}</div>
+			</div>
+		`,
+
+		data() { return { user: null, guest: null }},
+	};
+}
