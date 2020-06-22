ecf37af698b4f1ea274b3605d07e5eedba9e34f5
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-22 11:52:49 -0700

    feat(back arrow drawer): add back arrow drawer component

---
+++ components/back-arrow-drawer.vue
@@ -0,0 +1,41 @@
+<template>
+	<div :class="$style.root">
+		<div :class="$style.header">
+			<back-arrow-icon :class="$style.backButton" @click.native="$emit('close')" />
+			<slot name="header-item" />
+		</div>
+
+		<slot name="main-content"/>
+	</div>
+</template>
+
+<script>
+	import BackArrowIcon from './icons/back-arrow-icon';
+
+	export default {
+		components: { BackArrowIcon },
+	};
+</script>
+
+<style module lang="scss">
+	@import "../styles/variables.scss";
+	@import "../styles/mixins.scss";
+
+	$header-height: $spacing-06;
+
+	.root {
+		margin: $spacing-06 $spacing-07;
+	}
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: $spacing-06;
+	}
+
+	.backButton {
+		height: $header-height;
+		width: $header-height;
+	}
+</style>
