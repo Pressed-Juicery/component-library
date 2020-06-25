5cbecf807f8382e1f2e18a5489f1aa8179fcada6
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-24 18:46:21 -0700

    feat(wallet drawer): add imports, components, and props

--- components/wallet-drawer.vue
+++ components/wallet-drawer.vue
@@ -5,9 +5,17 @@
 </template>
 
 <script>
+import QrCode from './qr-code';
 
 export default {
+	components: { QrCode },
 
+	props: {
+		wallet: {
+			type: Object,
+			required: true,
+		},
+	},
 }
 </script>
 
