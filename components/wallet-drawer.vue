7cfeae52ef03a9b627f5ef4545a148b76b196e6b
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-25 08:55:20 -0700

    feat(wallet drawer): add main content

--- components/wallet-drawer.vue
+++ components/wallet-drawer.vue
@@ -1,11 +1,23 @@
 <template>
 	<div>
 
+		<div :class="$style.grid">
+			<qr-code :code="wallet.cardNumber" />
+			<div>
+				<div :class="$style.label">Current Balance</div>
+				<div :class="$style.value">{{ formatCurrency(wallet.funds) }}</div>
+				<div :class="$style.label">Points Accrued</div>
+				<div :class="$style.value">{{ wallet.points }}</div>
+				<div :class="$style.label">Account #</div>
+				<div :class="$style.value"></div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import QrCode from './qr-code';
+import { formatCurrency } from '../utilities/formatters';
 
 export default {
 	components: { QrCode },
@@ -14,6 +26,12 @@ export default {
 		wallet: {
 			type: Object,
 			required: true,
+		},
+	},
+
+	methods: {
+		formatCurrency(number) {
+			return formatCurrency(number);
 		},
 	},
 }
