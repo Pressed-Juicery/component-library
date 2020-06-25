97a63605798a9a2677742c502960eb4fddabedad
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-24 18:43:27 -0700

    feat(qr code): add qr code component

---
+++ components/qr-code.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<img v-if="src" :src="src" alt="qr code" />
+	</div>
+</template>
+
+<script>
+import QRCode from "qrcode";
+
+export default {
+	props : {
+		code : String,
+	},
+
+	data() {
+		return {
+			src : null,
+		};
+	},
+
+	created() {
+		this.generateQR(this.code);
+	},
+
+	methods : {
+		async generateQR(text) {
+			this.src = await QRCode.toDataURL(text, { type: 'image/png' });
+		},
+	},
+}
+</script>
