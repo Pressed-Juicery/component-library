b61368fdf36ae4e6586dec752d9079012d8e07e2
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-25 18:10:09 -0700

    feat(persistent drawer): add persistent drawer component

---
+++ components/persistent-drawer.vue
@@ -0,0 +1,66 @@
+<template>
+	<div :class="[{ [$style.isOpen]: isOpen }, $style.root]">
+		<div :class="$style.overlay" @click="close" />
+
+		<div :class="[{ [$style.hidden]: !isOpen }, $style.drawer]">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			isOpen: {
+				type: Boolean,
+				require: true,
+			},
+		},
+
+		methods: {
+			close() {
+				this.$emit('close');
+			},
+		},
+	};
+</script>
+
+<style module lang="scss">
+	@import "../styles/variables.scss";
+	@import "../styles/mixins.scss";
+
+	.root,
+	.overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	.overlay {
+		z-index: 1;
+		background-color: $gray-90;
+		opacity: 0;
+		pointer-events: none;
+		transition: .5s ease opacity;
+	}
+
+	.isOpen .overlay {
+		opacity: 0.2;
+		pointer-events: auto;
+	}
+
+	.drawer {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		max-height: 82%;
+		border-radius: $spacing-05 $spacing-05 0 0;
+		background-color: $white;
+		box-shadow: 0 1px 15px -8px rgba(0, 0, 0, 0.5);
+		z-index: 1;
+		overflow: auto;
+	}
+</style>
