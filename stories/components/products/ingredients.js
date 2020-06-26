958448896bc8a95770564a202100199a723d7b6a
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-06-26 16:45:39 -0700

    style(ingredients story): use one-true-brace-style

--- stories/components/products/ingredients.js
+++ stories/components/products/ingredients.js
@@ -5,35 +5,28 @@ export default {
 	component: Ingredients,
 };
 
-// eslint-disable-next-line max-lines-per-function
 export function Overview() {
 	return {
 		components: { Ingredients },
 		template: '<ingredients :ingredients="ingredients" />',
-		data() { // eslint-disable-line max-lines-per-function
+		data() {
 			return {
-				ingredients: [
-					{
-						title: 'Water',
-						iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/filtered-water.svg',
-					},
-					{
-						title: 'Dates',
-						iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/dates.svg',
-					},
-					{
-						title: 'Almonds (Pressed Nuts)',
-						iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/almonds-(pressed-nuts).svg',
-					},
-					{
-						title: 'Vanilla Extract',
-						iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/vanilla-extract.svg',
-					},
-					{
-						title: 'Sea Salt',
-						iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/sea-salt.svg',
-					},
-				],
+				ingredients: [{
+					title: 'Water',
+					iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/filtered-water.svg',
+				}, {
+					title: 'Dates',
+					iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/dates.svg',
+				}, {
+					title: 'Vanilla Extract',
+					iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/vanilla-extract.svg',
+				}, {
+					title: 'Almonds (Pressed Nuts)',
+					iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/almonds-(pressed-nuts).svg',
+				}, {
+					title: 'Sea Salt',
+					iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/ingredients/sea-salt.svg',
+				}],
 			};
 		},
 	};
