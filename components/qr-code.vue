fee6d08e1d4592a6b1252664df479f72a3004bcd
Silas Langley <silas.langley@pressedjuicery.com>
2020-06-26 15:00:25 -0700

    feat(qr code): add qr code component

---
+++ components/qr-code.vue
@@ -0,0 +1,69 @@
+<template>
+	<div>
+		<img :class="$style.largeQrCode" v-if="largeQrSrc" :src="largeQrSrc" alt="qr code" />
+		<img :class="$style.smallQrCode" v-if="smallQrSrc" :src="smallQrSrc" alt="qr code" />
+	</div>
+</template>
+
+<script>
+import QRCode from 'qrcode';
+
+export default {
+	props: {
+		code: {
+			type: String,
+			required: true,
+		},
+		background: {
+			type: String,
+			default: '#fff',
+		},
+	},
+
+	data() {
+		return {
+			src: null,
+			largeQrSrc: null,
+			smallQrSrc: null,
+		};
+	},
+
+	created() {
+		this.generateQR(this.code);
+	},
+
+	methods: {
+		async generateQR(text) {
+			const color = {
+				dark: '#000',
+				light: this.background,
+			};
+
+			this.smallQrSrc = await QRCode.toDataURL(text, { color, margin: 0, width: 130, type: 'image/png' });
+			this.largeQrSrc = await QRCode.toDataURL(text, { color, margin: 0, width: 167, type: 'image/png' });
+		},
+	},
+};
+</script>
+
+<style module lang="scss">
+	.largeQrCode {
+		display: block;
+	}
+
+	.smallQrCode {
+		display: none;
+	}
+
+	@media (max-width: 420px) {
+		.largeQrCode {
+			display: none;
+			margin-left: auto;
+		}
+
+		.smallQrCode {
+			display: block;
+			margin-left: auto;
+		}
+	}
+</style>
