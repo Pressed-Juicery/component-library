04deb164925388052d956dd62a930e6a1fbb30d8
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-06-29 10:18:12 -0700

    feat(prevent-body-scrolling): add `preventAncestorScrolling` function

--- utilities/prevent-body-scrolling.js
+++ utilities/prevent-body-scrolling.js
@@ -3,3 +3,23 @@ export function preventBodyScrolling(shouldPreventScrolling) {
 
 	window.document.body.classList[action]('prevent-scrolling');
 }
+
+export function preventAncestorScrolling(startingElement) {
+	const body = document.body;
+	let currentElement = startingElement && startingElement.parentElement;
+
+	while (currentElement) {
+		const isPositioned = getComputedStyle(currentElement).position !== 'static';
+
+		if (isPositioned || currentElement === body) {
+			currentElement.classList.add('prevent-scrolling');
+			break;
+		}
+
+		currentElement = currentElement.parentElement;
+	}
+
+	return function allowScrolling() {
+		currentElement.classList.remove('prevent-scrolling');
+	};
+}
