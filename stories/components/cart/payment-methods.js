5fc7c1999521fafec0b9ddb38531cb63389d36ad
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-02 15:38:02 -0700

    refactor(payment-methods story): inline variables

--- stories/components/cart/payment-methods.js
+++ stories/components/cart/payment-methods.js
@@ -5,36 +5,6 @@ export default {
 	component: PaymentMethods,
 };
 
-const mastercard = {
-	identifier: '····6789',
-	vendor: 'mastercard',
-};
-
-const visa = {
-	identifier: '····4321',
-	vendor: 'visa',
-};
-
-const visa2 = {
-	identifier: '····9360',
-	vendor: 'visa',
-};
-
-const discover = {
-	identifier: '····1111',
-	vendor: 'discover',
-};
-
-const americanExpress = {
-	identifier: '····2222',
-	vendor: 'american express',
-};
-
-const unknown = {
-	identifier: '····3563',
-	vendor: 'unknown',
-};
-
 export function Overview() {
 	return {
 		components: { PaymentMethods },
@@ -42,12 +12,12 @@ export function Overview() {
 		data() {
 			return {
 				paymentMethods: [
-					mastercard,
-					visa,
-					visa2,
-					discover,
-					americanExpress,
-					unknown,
+					{ identifier: '····6789', vendor: 'mastercard' },
+					{ identifier: '····4321', vendor: 'visa' },
+					{ identifier: '····9360', vendor: 'visa' },
+					{ identifier: '····1111', vendor: 'discover' },
+					{ identifier: '····2222', vendor: 'american express' },
+					{ identifier: '····3563', vendor: 'unknown' },
 				],
 			};
 		},
