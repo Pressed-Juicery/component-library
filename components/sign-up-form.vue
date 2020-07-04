4d2818bb081b71058c8ac755eec3de380fed8a08
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-03 22:21:02 -0700

    feat(sign-up-form): add input types to email and phone fields

--- components/sign-up-form.vue
+++ components/sign-up-form.vue
@@ -3,13 +3,11 @@
 		<div :class="$style.row">
 			<validated-input
 				label="First Name"
-				:class="$style.inputHalf"
 				:rules="rules.firstName"
 				v-model="user.firstName"
 			/>
 			<validated-input
 				label="Last Name"
-				:class="$style.inputHalf"
 				:rules="rules.lastName"
 				v-model="user.lastName"
 			/>
@@ -17,7 +15,7 @@
 		<validated-input type="email" label="Email" :rules="rules.email" v-model="user.email" />
 		<validated-input type="password" label="Password" :rules="rules.password" v-model="user.password" />
 		<validated-input type="date" label="Birthday (optional)" :rules="rules.birthday" v-model="user.birthday" />
-		<validated-input type="phone" label="Phone Number" :rules="rules.phone" v-model="user.phone" />
+		<validated-input type="tel" label="Phone Number" :rules="rules.phone" v-model="user.phone" />
 	</validated-form>
 </template>
 
@@ -85,18 +83,8 @@ export default {
 	@import '../styles/variables';
 
 	.row {
-		display: flex;
-	}
-
-	.inputHalf {
-		flex: 1 1 0;
-	}
-
-	.inputHalf:first-of-type {
-		margin-right: $spacing-05;
-	}
-
-	.inputHalf:nth-of-type(2) {
-		margin-left: $spacing-05;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: $spacing-07;
 	}
 </style>
