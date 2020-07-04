08bf4630f096c6154e7296d7dff705a66f324092
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-03 19:17:57 -0700

    feat(item-card stories): add cart item modifier examples

--- stories/components/cart/cart-item-card.js
+++ stories/components/cart/cart-item-card.js
@@ -1,4 +1,5 @@
 import CartItemCard from '../../../components/cart-item-card';
+import { cartItemModifiers } from '../../../demo-data/cart-item-modifiers';
 
 export default {
 	title: 'Components / Cart / CartItemCard',
@@ -59,10 +60,7 @@ export function AdditionalInformation() {
 						name: 'Strawberry Almond',
 						imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/STLEM_43924384-906a-4aaa-a74f-6fcb8fb22cfb_large.png?v=1565306900',
 					},
-					modifiers: [{
-						groupName: 'Toppings',
-						name: 'Chocolate chip cookies, granola, chocolate chips',
-					}],
+					modifiers: cartItemModifiers,
 					quantity: 3,
 					price: 6.50,
 					originalPrice: 6.50,
