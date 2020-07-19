ce3a7f67e011e3dc0fc9f03525afbbc91b48effe
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-19 11:14:11 -0700

    feat(confirm-drawer story): remove unnecessary wrapper element

--- stories/components/basic/confirm-drawer.js
+++ stories/components/basic/confirm-drawer.js
@@ -20,11 +20,9 @@ export function Overview() {
 				:isOpen="isOpen"
 				@close="closeDrawer"
 			>
-				<div>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. <a style="text-decoration: underline;" href="#">Click Here</a>
-				</div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+				aliquip ex ea commodo consequat. <a style="text-decoration: underline;" href="#">Click Here</a>
 			</confirm-drawer>
 		`,
 		props: {
