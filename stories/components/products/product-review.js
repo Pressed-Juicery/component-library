dbd4c177f1efb247dbe54db5b768d4cb125be4f9
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-19 12:12:13 -0700

    feat(product-review story): update example data

--- stories/components/products/product-review.js
+++ stories/components/products/product-review.js
@@ -14,13 +14,13 @@ export function Overview() {
 				review: {
 					title: 'Test',
 					body: `
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-						sed do eiusmod tempor incididunt ut labore et dolore magna
-						aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-						ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
-					rating: 3.68,
-					name: 'First Last',
-					createdAt: '01/01/2020',
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat.
+					`,
+					rating: 4,
+					name: 'Jane Doe',
+					createdAt: 'Jan 1, 2020',
 				},
 			};
 		},
