6e490f10a1b396287aaccef8d07c013b56650262
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-19 11:08:30 -0700

    feat(confirm-drawer story): change `href` in demonstration link

--- stories/components/basic/confirm-drawer.js
+++ stories/components/basic/confirm-drawer.js
@@ -23,8 +23,7 @@ export function Overview() {
 				<div>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
 					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat.
-					<a style="text-decoration: underline;" href="https://pressedjuicery.com/">Click Here</a>
+					aliquip ex ea commodo consequat. <a style="text-decoration: underline;" href="#">Click Here</a>
 				</div>
 			</confirm-drawer>
 		`,
