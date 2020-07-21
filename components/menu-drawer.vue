4bd2daa5e156e628637e8307ebdff777d56c7c4f
Silas Langley <silas.langley@pressedjuicery.com>
2020-07-21 10:41:09 -0700

    feat(menu drawer): add menu drawer component

---
+++ components/menu-drawer.vue
@@ -0,0 +1,40 @@
+<template>
+	<drawer :is-open="isOpen" @close="close">
+		<menu-dialog-template
+			heading="Menu Dialog Template"
+			:menuItems="menuItems"
+			@close="close"
+		/>
+	</drawer>
+</template>
+
+<script>
+import Drawer from './drawer.vue';
+import MenuDialogTemplate from './menu-dialog-template.vue';
+
+export default {
+	components: { Drawer, MenuDialogTemplate },
+
+	props: {
+		isOpen: {
+			type: Boolean,
+			require: true,
+		},
+
+		heading: {
+			type: String,
+		},
+
+		menuItems: {
+			type: Array,
+			require: true,
+		},
+	},
+
+	methods: {
+		close() {
+			this.isOpen = false;
+		},
+	},
+};
+</script>
