9a37186590d2e535132a47583f6a2521518273a9
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-07-27 11:29:28 -0700

    style(product-ordering-section story): use 1TBS (one true brace style)

--- stories/components/products/product-ordering-section.js
+++ stories/components/products/product-ordering-section.js
@@ -14,29 +14,25 @@ export function Overview() {
 				isVip: false,
 				product: {
 					name: 'Vanilla Freeze',
-					variants: [
-						{
-							id: 1,
-							name: 'Size 1',
-							calories: '200',
-							nonMemberPrice: '5.00',
-							memberPrice: '6.50',
-						},
-						{
-							id: 2,
-							name: 'Size 2',
-							calories: '400',
-							nonMemberPrice: '8.50',
-							memberPrice: '7.00',
-						},
-						{
-							id: 3,
-							name: 'Size 3',
-							calories: '100',
-							nonMemberPrice: '4.00',
-							memberPrice: '4.00',
-						},
-					],
+					variants: [{
+						id: 1,
+						name: 'Size 1',
+						calories: '200',
+						nonMemberPrice: '5.00',
+						memberPrice: '6.50',
+					}, {
+						id: 2,
+						name: 'Size 2',
+						calories: '400',
+						nonMemberPrice: '8.50',
+						memberPrice: '7.00',
+					}, {
+						id: 3,
+						name: 'Size 3',
+						calories: '100',
+						nonMemberPrice: '4.00',
+						memberPrice: '4.00',
+					}],
 				},
 			};
 		},
