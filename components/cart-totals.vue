075eff765f8ea29567f4e268dbf2d89afde92740
spencer <slalexander92@gmail.com>
2020-08-05 18:14:37 -0700

    feat(cart-totals): simplify conditions for displaying delivery tip info

--- components/cart-totals.vue
+++ components/cart-totals.vue
@@ -42,7 +42,7 @@
 			</div>
 			<div v-else>Shipping/Delivery</div>
 
-			<div v-if="isLocalDelivery && deliveryTip" :class="$style.rowGroup">
+			<div v-if="deliveryTip" :class="$style.rowGroup">
 				<div :class="$style.originalSubtotal">{{ formatCurrency(cart.originalFulfillmentPrice) }}</div>
 				<div>FREE</div>
 			</div>
@@ -56,8 +56,8 @@
 			<div v-else :class="$style.fulfillmentInfo">calculated at next step</div>
 		</div>
 
-		<div v-if="isLocalDelivery && deliveryTip">
-			<div v-if="isTipSummaryOpen && deliveryTip" :class="$style.row">
+		<div v-if="deliveryTip">
+			<div v-if="isTipSummaryOpen" :class="$style.row">
 				<div :class="[$style.row, $style.rowGroup, $style.discountAmount]">
 					<div>Free local delivery (just pay tip)</div>
 					<div :class="$style.discountAmount">
@@ -65,11 +65,10 @@
 					</div>
 				</div>
 			</div>
-		</div>
-
-		<div v-if="isLocalDelivery && deliveryTip" :class="[$style.row, $style.rowGroup]">
-			<div>Delivery Tip</div>
-			<div>{{ formatCurrency(deliveryTip) }}</div>
+			<div :class="[$style.row, $style.rowGroup]">
+				<div>Delivery Tip</div>
+				<div>{{ formatCurrency(deliveryTip) }}</div>
+			</div>
 		</div>
 
 		<div :class="[$style.totalRow, $style.rowGroup]">
