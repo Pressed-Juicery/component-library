d1a90d632a5bc8b7999b96e927bacc4696b688b1
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-08-16 21:07:00 -0700

    feat(product-ordering-section): illustrate display of calories

--- stories/components/products/product-ordering-section.js
+++ stories/components/products/product-ordering-section.js
@@ -36,7 +36,7 @@ export function Overview() { // eslint-disable-line max-lines-per-function
 						`,
 						imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/Frozenmini_front_matcha_1440x1440_c08848fa-50f4-4d8d-9370-9c851c0100b2.png?v=1597174401',
 						ingredients: [],
-						nutritionSummary: null,
+						nutritionSummary: { calories: 200 },
 					},
 				}, {
 					name: 'Matcha Frozen Mini - 6 Count',
@@ -54,7 +54,7 @@ export function Overview() { // eslint-disable-line max-lines-per-function
 						`,
 						imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/Frozenmini_6pack__front_matcha_1440x1440_3f4cb9ab-348e-4429-886c-0037ee29de53.png?v=1597168136',
 						ingredients: [],
-						nutritionSummary: null,
+						nutritionSummary: { calories: 200 },
 					},
 				}],
 			};
