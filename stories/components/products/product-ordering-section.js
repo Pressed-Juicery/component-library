8bf02ea623c054840f072f44ec3ad62fa8237f62
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-08-16 20:42:33 -0700

    feat(product-ordering-section story): show variant image change
    The previous example used the same image url for, both, the single
    and 6 count celery juice bundle. The bundle image was no longer
    available. Therefore, the example was changed to Matcha Frozen Minis.

--- stories/components/products/product-ordering-section.js
+++ stories/components/products/product-ordering-section.js
@@ -21,34 +21,38 @@ export function Overview() { // eslint-disable-line max-lines-per-function
 				isVip: false,
 				current: null,
 				options: [{
-					name: 'Celery Juice - Single',
+					name: 'Matcha Frozen Mini - Single',
 					value: {
-						name: 'Celery Juice - Single',
-						nonMemberPrice: 6.5,
-						memberPrice: 5,
+						name: 'Matcha Frozen Mini - Single',
+						nonMemberPrice: 5,
+						memberPrice: 4.5,
 						nonMemberSalePrice: null,
 						memberSalePrice: null,
 						shortDescription: `
-							Try our natural celery juice as an alternative to a morning coffee. It packed with 15
-							vitamins & minerals and helps flush out toxins, lower blood pressure.
+							At only 190 calories per cup, this frozen dessert is made from simple ingredients like
+							coconut cream, almonds, and matcha with no refined sugars, this decadent treat is a
+							plant-based delight and the perfect portion size for an after-dinner treat.
+							(Some say better than ice cream!)
 						`,
-						imageUrl: 'https://cdn.shopify.com/s/files/1/0022/5601/1299/products/Celery.png?v=1563907503',
+						imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/Frozenmini_front_matcha_1440x1440_c08848fa-50f4-4d8d-9370-9c851c0100b2.png?v=1597174401',
 						ingredients: [],
 						nutritionSummary: null,
 					},
 				}, {
-					name: 'Celery Juice - 6 Pack',
+					name: 'Matcha Frozen Mini - 6 Count',
 					value: {
-						name: 'Celery Juice - Single',
+						name: 'Matcha Frozen Mini - 6 Count',
 						nonMemberPrice: 30,
-						memberPrice: 20,
+						memberPrice: 25,
 						nonMemberSalePrice: null,
 						memberSalePrice: null,
 						shortDescription: `
-							Try our natural celery juice as an alternative to a morning coffee. It packed with 15
-							vitamins & minerals and helps flush out toxins, lower blood pressure.
+							At only 190 calories per cup, this frozen dessert is made from simple ingredients like
+							coconut cream, almonds, and matcha with no refined sugars, this decadent treat is a
+							plant-based delight and the perfect portion size for an after-dinner treat.
+							(Some say better than ice cream!)
 						`,
-						imageUrl: 'https://cdn.shopify.com/s/files/1/0022/5601/1299/products/Celery.png?v=1563907503',
+						imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/Frozenmini_6pack__front_matcha_1440x1440_3f4cb9ab-348e-4429-886c-0037ee29de53.png?v=1597168136',
 						ingredients: [],
 						nutritionSummary: null,
 					},
