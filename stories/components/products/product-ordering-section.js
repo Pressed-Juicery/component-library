caacf1efec7f3f1a64264687bc4938e369af9b58
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-08-16 20:28:31 -0700

    style(product-ordering-section story): use 1TBS (one true brace style)

--- stories/components/products/product-ordering-section.js
+++ stories/components/products/product-ordering-section.js
@@ -20,44 +20,41 @@ export function Overview() { // eslint-disable-line max-lines-per-function
 			return {
 				isVip: false,
 				current: null,
-				options: [
-					{
+				options: [{
+					name: 'Celery Juice - Single',
+					value: {
 						name: 'Celery Juice - Single',
-						value: {
-							name: 'Celery Juice - Single',
-							nonMemberPrice: 6.5,
-							memberPrice: 5,
-							nonMemberSalePrice: null,
-							memberSalePrice: null,
-							shortDescription: `Try our natural
-								celery juice as an alternative to a
-								morning coffee. It packed with 15
-								vitamins & minerals and helps flush
-								out toxins, lower blood pressure.`,
-							imageUrl: 'https://cdn.shopify.com/s/files/1/0022/5601/1299/products/Celery.png?v=1563907503',
-							ingredients: [],
-							nutritionSummary: null,
-						},
+						nonMemberPrice: 6.5,
+						memberPrice: 5,
+						nonMemberSalePrice: null,
+						memberSalePrice: null,
+						shortDescription: `Try our natural
+							celery juice as an alternative to a
+							morning coffee. It packed with 15
+							vitamins & minerals and helps flush
+							out toxins, lower blood pressure.`,
+						imageUrl: 'https://cdn.shopify.com/s/files/1/0022/5601/1299/products/Celery.png?v=1563907503',
+						ingredients: [],
+						nutritionSummary: null,
 					},
-					{
-						name: 'Celery Juice - 6 Pack',
-						value: {
-							name: 'Celery Juice - Single',
-							nonMemberPrice: 30,
-							memberPrice: 20,
-							nonMemberSalePrice: null,
-							memberSalePrice: null,
-							shortDescription: `Try our natural
-								celery juice as an alternative to a
-								morning coffee. It packed with 15
-								vitamins & minerals and helps flush
-								out toxins, lower blood pressure.`,
-							imageUrl: 'https://cdn.shopify.com/s/files/1/0022/5601/1299/products/Celery.png?v=1563907503',
-							ingredients: [],
-							nutritionSummary: null,
-						},
+				}, {
+					name: 'Celery Juice - 6 Pack',
+					value: {
+						name: 'Celery Juice - Single',
+						nonMemberPrice: 30,
+						memberPrice: 20,
+						nonMemberSalePrice: null,
+						memberSalePrice: null,
+						shortDescription: `Try our natural
+							celery juice as an alternative to a
+							morning coffee. It packed with 15
+							vitamins & minerals and helps flush
+							out toxins, lower blood pressure.`,
+						imageUrl: 'https://cdn.shopify.com/s/files/1/0022/5601/1299/products/Celery.png?v=1563907503',
+						ingredients: [],
+						nutritionSummary: null,
 					},
-				],
+				}],
 			};
 		},
 		methods: {
