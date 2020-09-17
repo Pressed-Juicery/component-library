9ffd8057373b0b9f277871bbde13f0a5fc0d52d2
Vinni Hoke <vinni.hoke@gmail.com>
2020-09-17 09:40:12 -0700

    style(product-ordering-section): changing margin to padding

--- components/product-ordering-section.vue
+++ components/product-ordering-section.vue
@@ -199,7 +199,7 @@ export default {
 		display: grid;
 		grid-template-columns: $spacing-11 1fr;
 		grid-gap: $spacing-03;
-		margin-bottom: $spacing-06;
+		padding-bottom: $spacing-06;
 	}
 
 	.quantity {
