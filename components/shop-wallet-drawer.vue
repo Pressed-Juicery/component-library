1bb1a615ed5d46bc1bf4bd681be516323a101958
spencer <slalexander92@gmail.com>
2020-09-18 13:51:56 -0700

    feat(shop-wallet-drawer): add custom-header with pressed-points-circle component

--- components/shop-wallet-drawer.vue
+++ components/shop-wallet-drawer.vue
@@ -3,20 +3,38 @@
 		:isOpen="isOpen"
 		@close="close"
 	>
+		<template #custom-header>
+			<pressed-points-circle :class="$style.logo"/>
+		</template>
 
 	</collapsible-drawer>
 </template>
 
 <script>
 import CollapsibleDrawer from './collapsible-drawer';
+import PressedPointsCircle from './icons/pressed-points-circle';
+
 export default {
 	components: {
 		CollapsibleDrawer,
+		PressedPointsCircle,
 	},
 
 }
 </script>
 
 <style>
+<style module lang="scss">
+	@import "../styles/mixins";
+	.logo {
+		position: relative;
+		display: block;
+		top: (-$logoOffset);
+		height: 66px;
+		width: 66px;
+		margin: 0 auto (-$logoOffset + $spacing-05);
+		opacity: 0;
+		transition: opacity .5s ease;
+	}
 
 </style>
