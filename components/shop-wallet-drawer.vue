32fe9b11df90210c4cb9bddf01a14a5e2fa6a271
spencer <slalexander92@gmail.com>
2020-09-18 13:50:07 -0700

    feat(shop-wallet-drawer): add collapsible drawer

--- components/shop-wallet-drawer.vue
+++ components/shop-wallet-drawer.vue
@@ -1,9 +1,18 @@
 <template>
+	<collapsible-drawer
+		:isOpen="isOpen"
+		@close="close"
+	>
 
+	</collapsible-drawer>
 </template>
 
 <script>
+import CollapsibleDrawer from './collapsible-drawer';
 export default {
+	components: {
+		CollapsibleDrawer,
+	},
 
 }
 </script>
