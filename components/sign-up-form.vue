0cd50b41a233b85df93e8f414506cf97aa7554af
Vinni Hoke <vinni.hoke@gmail.com>
2020-09-22 17:18:30 -0700

    feat(input-type-check): autocomplete added to signup-form

--- components/sign-up-form.vue
+++ components/sign-up-form.vue
@@ -3,25 +3,44 @@
 		<div :class="$style.row">
 			<validated-input
 				type="text"
+				autocomplete="given-name"
 				label="First Name"
 				:rules="rules.firstName"
 				v-model="user.firstName"
 			/>
 			<validated-input
 				type="text"
+				autocomplete="family-name"
 				label="Last Name"
 				:rules="rules.lastName"
 				v-model="user.lastName"
 			/>
 		</div>
-		<validated-input type="email" label="Email" :rules="rules.email" v-model="user.email" />
-		<validated-input type="password" label="Password" :rules="rules.password" v-model="user.password" />
+		<validated-input
+			type="email"
+			autocomplete="email"
+			label="Email"
+			:rules="rules.email"
+			v-model="user.email"
+		/>
+		<validated-input
+			type="password"
+			label="Password"
+			:rules="rules.password"
+			v-model="user.password"
+		/>
 		<div :class="$style.birthdayWrapper">
 			<div :class="$style.birthdayLabel">Birthday (optional)</div>
 			<div :class="$style.birthdayMessage">So we can send you something sweet!</div>
 			<validated-birthday v-model="user.birthday" />
 		</div>
-		<validated-input type="tel" label="Phone Number" :rules="rules.phone" v-model="user.phone" />
+		<validated-input
+			type="tel"
+			autocomplete="tel"
+			label="Phone Number"
+			:rules="rules.phone"
+			v-model="user.phone"
+		/>
 	</validated-form>
 </template>
 
