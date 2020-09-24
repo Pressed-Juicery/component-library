3ef8845e23b83d2225483f048f2f5bfdaeff9894
Spencer <slalexander92@gmail.com>
2020-09-24 12:30:54 -0700

    feat(circle-arrow-right): add circle arrow icon

---
+++ components/icons/circle-arrow-right.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>learn more</title>
+      <g id="learn-more" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="Up-carrot" transform="translate(20.000000, 20.000000) rotate(-270.000000) translate(-20.000000, -20.000000) translate(-0.000000, -0.000000)" stroke="#D1D1D1">
+              <line x1="20" y1="19" x2="24" y2="15" id="Path" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" transform="translate(22.000000, 17.000000) rotate(-270.000000) translate(-22.000000, -17.000000) "></line>
+              <line x1="16" y1="15" x2="20" y2="19" id="Path" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" transform="translate(18.000000, 17.000000) rotate(-270.000000) translate(-18.000000, -17.000000) "></line>
+              <line x1="16" y1="21" x2="24" y2="21" id="Path" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" transform="translate(20.000000, 21.000000) rotate(-270.000000) translate(-20.000000, -21.000000) "></line>
+              <circle id="Oval" cx="20" cy="20" r="19.5"></circle>
+          </g>
+      </g>
+  </svg>
+</template>
