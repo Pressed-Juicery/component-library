6e3cc51341da14c6d741d6a7883091c6c3e056b2
Spencer <slalexander92@gmail.com>
2020-09-28 10:42:13 -0700

    feat(cart-summary story): add click event handler to story

--- stories/components/cart/cart-summary.js
+++ stories/components/cart/cart-summary.js
@@ -68,7 +68,7 @@ export function Overview() {
 export function AllowCartUpdates() {
 	return {
 		components: { CartSummary },
-		template: '<cart-summary :cart="cart" :can-update-quantity="true" @change="onChange"/>',
+		template: '<cart-summary :cart="cart" :can-update-quantity="true" @change="onChange" @click="handleClick" />',
 		data() {
 			return { cart };
 		},
@@ -77,6 +77,10 @@ export function AllowCartUpdates() {
 				// eslint-disable-next-line no-console
 				console.log(`Changed item quantity for "${item.name}" to ${item.quantity}`);
 			},
+
+			handleClick(product) {
+				console.log('product:', product); // TODO: REMOVE THIS
+			}
 		},
 	};
 }
