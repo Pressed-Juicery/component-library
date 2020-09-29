2f86d718983a90864d1385ffc01366e0dcaa8a12
Nick Herrera <nick.herrera@pressedjuicery.com>
2020-09-29 11:27:01 -0700

    fix(validated-input): reverse values of boolean assignements

--- components/validated-input.vue
+++ components/validated-input.vue
@@ -19,8 +19,8 @@
 				v-model="model"
 				@blur="isEager = true"
 			>
-			<eye-active v-if="shouldShowPassword" :class="$style.eye" @click.native="shouldShowPassword = true" />
-			<eye-inactive v-else :class="$style.eye" @click.native="shouldShowPassword = false" />
+			<eye-active v-if="shouldShowPassword" :class="$style.eye" @click.native="shouldShowPassword = false" />
+			<eye-inactive v-else :class="$style.eye" @click.native="shouldShowPassword = true" />
 		</div>
 	</validated-component>
 </template>
