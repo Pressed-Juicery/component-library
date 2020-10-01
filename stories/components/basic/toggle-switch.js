475ac5f2ec1c3c70626753a70ac76432b74927f5
Vinni Hoke <vinni.hoke@gmail.com>
2020-10-01 16:57:01 -0700

    style(toggle-switch): creating toggle-switch story

---
+++ stories/components/basic/toggle-switch.js
@@ -0,0 +1,43 @@
+import ToggleSwitch from '../../../components/toggle-switch';
+
+export default {
+	title: 'Components / Basic / ToggleSwitch',
+	component: ToggleSwitch,
+};
+
+export function Overview() {
+	return {
+		components: { ToggleSwitch },
+		template: '<toggle-switch :isOn="isOn" @change="handleToggle"/>',
+		data() {
+			return {
+				isOn: false,
+			};
+		},
+		methods: {
+			handleToggle() {
+				this.isOn = !this.isOn;
+				console.log('Toggled'); // eslint-disable-line no-console
+			},
+		},
+	};
+}
+
+export function Disabled() {
+	return {
+		components: { ToggleSwitch },
+		template: '<toggle-switch :isOn="isOn" @on="handleToggle" :isDisabled="isDisabled"/>',
+		data() {
+			return {
+				isOn: false,
+				isDisabled: true,
+			};
+		},
+		methods: {
+			handleToggle() {
+				this.isOn = !this.isOn;
+				console.log('Toggled'); // eslint-disable-line no-console
+			},
+		},
+	};
+}
