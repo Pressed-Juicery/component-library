9ab64ffe7b95e98194d371402351e64b37112f85
Vinni Hoke <vinni.hoke@gmail.com>
2020-10-01 16:57:19 -0700

    feat(toggle-switch): creating toggle-switch

---
+++ components/toggle-switch.vue
@@ -0,0 +1,71 @@
+<template>
+	<div
+		:class="[[isOn ? $style.switchOn : $style.switch], {[$style.disabled]: isDisabled}]"
+		@click="!isDisabled && $emit('change')"
+	>
+		<div :class="isOn ? $style.innerDotOn : $style.innerDot" />
+	</div>
+</template>
+
+<script>
+
+export default {
+	props: {
+		isOn: {
+			type: Boolean,
+			required: true,
+		},
+		isDisabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+};
+</script>
+
+<style lang="scss" module>
+	@import '../styles/variables';
+
+	$switch-size: $spacing-07;
+
+	.switch,
+	.switchOn {
+		position: relative;
+		width: 58px;
+		height: $switch-size;
+		border-radius: $switch-size;
+		border: 1px solid $color-primary;
+		cursor: pointer;
+	}
+
+	.disabled {
+		filter: invert(70%);
+		cursor: not-allowed;
+	}
+
+	.switchOn,
+	.innerDot {
+		background-color: $color-primary;
+	}
+
+	$dot-absolute-positioning: 2px;
+	$dot-size: 26px;
+
+	.innerDot,
+	.innerDotOn {
+		top: $dot-absolute-positioning;
+		position: absolute;
+		border-radius: 50%;
+		height: $dot-size;
+		width:$dot-size;
+	}
+
+	.innerDot {
+		left: $dot-absolute-positioning;
+	}
+
+	.innerDotOn {
+		right: $dot-absolute-positioning;
+		background-color: $white;
+	}
+</style>
