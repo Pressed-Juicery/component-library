768c84ce23b89c3ef35c69722d3d6fce9b2c8a94
Spencer <slalexander92@gmail.com>
2020-10-02 14:25:07 -0700

    feat(subscription-form): import required components

--- components/subscription-form.vue
+++ components/subscription-form.vue
@@ -2,7 +2,20 @@
 </template>
 
 <script>
+import ArrowDownIcon from './icons/arrow-down-icon';
+import Card from './card';
+import PaymentMethodRadioButtonCard from './payment-method-radio-button-card';
+import ValidatedCheckbox from './validated-checkbox';
+import ValidatedForm from './validated-form';
+
 export default {
+  components: {
+    ArrowDownIcon,
+    Card,
+    PaymentMethodRadioButtonCard,
+    ValidatedCheckbox,
+    ValidatedForm,
+  },
 
 }
 </script>
