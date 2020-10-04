13081ebe6960adbc878d634d7f4e415759d7845f
Spencer <slalexander92@gmail.com>
2020-10-04 12:03:27 -0700

    feat(subscription-form): add radio button cards

--- components/subscription-form.vue
+++ components/subscription-form.vue
@@ -14,6 +14,12 @@
 		</div>
 
 		<div :class="$style.label">Payment Method</div>
+		<payment-method-radio-button-card
+			v-for="method in paymentMethods"
+			v-model="selectedPaymentMethod"
+			:paymentMethod="method"
+			:key="method.id"
+		/>
 
 	</validated-form>
 </template>
