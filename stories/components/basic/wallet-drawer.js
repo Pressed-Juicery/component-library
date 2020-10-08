d120e1ec77e8d7ac991f03d657459e3b5ffadf3f
Spencer <slalexander92@gmail.com>
2020-10-08 14:55:04 -0700

    feat(wallet-drawer): add vip story

--- stories/components/basic/wallet-drawer.js
+++ stories/components/basic/wallet-drawer.js
@@ -30,3 +30,30 @@ export function Overview() {
 		},
 	};
 }
+
+export function VIP() {
+	return {
+		components: { WalletDrawer },
+		template: `
+			<div style="position:fixed;bottom:0;left:0;right:0;">
+				<wallet-drawer :wallet="wallet" @reload="onReload"/>
+			</div>
+		`,
+
+		data() {
+			return {
+				wallet: {
+					cardNumber: '411111111111',
+					points: 250,
+					funds: 50.25,
+					tier: 'vip',
+				},
+			};
+		},
+		methods: {
+			onReload() {
+				console.log('Reload button clicked'); // eslint-disable-line no-console
+			},
+		},
+	};
+}
