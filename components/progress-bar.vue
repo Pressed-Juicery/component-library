396340c4d03a2c97e973eeaca37f27d154491ded
Spencer Alexander <slalexander92@gmail.com>
2020-11-19 08:47:06 -0800

    feat(progress-bar): add progress-bar component

---
+++ components/progress-bar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="$style.root">
+    <div :class="$style.progressBar" :style="{ width: fill, backgroundColor: barColor }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    barColor: {
+      type: String,
+      default: '#bf9033',
+    },
+    goal: Number,
+    progress: Number,
+  },
+
+  computed: {
+    fill() {
+      return `${(this.progress / this.goal) * 100}%`;
+    },
+  },
+}
+</script>
+
+<style module lang="scss">
+  @import '../styles/variables';
+
+  $barBackground: #e9e0c3;
+
+  .root {
+    position: relative;
+    height: $spacing-02;
+    border-radius: $spacing-02;
+    background-color: $barBackground;
+  }
+
+  .progressBar {
+    position: absolute;
+    border-radius: $spacing-02;
+    left: 0;
+    height: 100%
+  }
+</style>
