1aa34dbe4d80517f120f548a9c25fa921a910f13
Spencer Alexander <slalexander92@gmail.com>
2020-11-19 08:52:11 -0800

    feat(cart-upsell): add basic cart-upsell component

---
+++ components/cart-upsell.vue
@@ -0,0 +1,22 @@
+<template>
+	<card v-if="upsell" :class="[[$style.root], {[$style.isOpen]: isOpen}]">
+	</card>
+</template>
+
+<script>
+import Card from './card';
+export default {
+	components: { Card },
+
+	props: {
+		upsell: {
+			type: Object,
+			required: true,
+		},
+		isOpen: Boolean,
+	},
+};
+</script>
+
+<style module lang="scss">
+</style>
