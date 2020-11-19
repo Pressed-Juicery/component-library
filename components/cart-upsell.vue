060553a2aa12350af7aebf44a96144d4c4dbb3b3
Spencer Alexander <slalexander92@gmail.com>
2020-11-19 08:53:15 -0800

    feat(cart-upsell): add header + styles

--- components/cart-upsell.vue
+++ components/cart-upsell.vue
@@ -1,12 +1,17 @@
 <template>
 	<card v-if="upsell" :class="[[$style.root], {[$style.isOpen]: isOpen}]">
+		<div :class="$style.header" @click="$emit('toggle')">
+			<div>{{ upsell && upsell.title }}</div>
+			<arrow-icon :class="$style.arrow" />
+		</div>
 	</card>
 </template>
 
 <script>
 import Card from './card';
+import ArrowIcon from './icons/arrow-down-icon';
 export default {
-	components: { Card },
+	components: { ArrowIcon, Card },
 
 	props: {
 		upsell: {
@@ -19,4 +24,22 @@ export default {
 </script>
 
 <style module lang="scss">
+	@import '../styles/mixins';
+
+	.header {
+		@include text-heading-6;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		cursor: pointer;
+	}
+
+	.arrow {
+		width: $spacing-04;
+	}
+
+	.isOpen .arrow{
+		transform: rotate(180deg);
+	}
+
 </style>
