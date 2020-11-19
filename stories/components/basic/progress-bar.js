65de99fff165e7465084f3672478a7f7d05455cd
Spencer Alexander <slalexander92@gmail.com>
2020-11-19 08:47:21 -0800

    feat(progress-bar story): add story for progress bar

---
+++ stories/components/basic/progress-bar.js
@@ -0,0 +1,22 @@
+import ProgressBar from '../../../components/progress-bar';
+
+export default {
+	title: 'Components / Basic / ProgressBar',
+	component: ProgressBar,
+};
+
+export function Overview() {
+	return {
+		components: { ProgressBar },
+		template: `
+			<progress-bar :goal="goal" :progress="progress"/>
+		`,
+
+		data() {
+			return {
+				goal: 100,
+				progress: 50
+			};
+		},
+	};
+}
