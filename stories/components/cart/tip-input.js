e8f7bc5cc40ce5bbabc31f72f07922e436529d23
Vinni <vinni.hoke@gmail.com>
2020-12-07 11:43:42 -0800

    feat(tip-input): adding a TipInput story

---
+++ stories/components/cart/tip-input.js
@@ -0,0 +1,31 @@
+import TipInput from '../../../components/tip-input.vue';
+
+export default {
+	title: 'Components / Cart / TipInput',
+	component: TipInput,
+};
+
+export function Overview() {
+	return {
+		components: { TipInput },
+		template: `
+		<div>
+			<tip-input :subtotal="subtotal" @change="handleChange">
+				<div>Tip Your Associate</div>
+			</tip-input>
+			Sample showing the current tip value: {{ tip }}
+		</div>
+		`,
+		data() {
+			return {
+				subtotal: 27.00,
+				tip: null,
+			};
+		},
+		methods: {
+			handleChange(value) {
+				this.tip = value;
+			},
+		},
+	};
+}
