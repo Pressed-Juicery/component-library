bfd0e9e851ddb28cd50a72a47501c3a38942a9a0
Spencer Alexander <slalexander92@gmail.com>
2020-12-23 09:41:55 -0800

    feat(cart-upsell-menu): pass wallet into cart-upsell-menu story

--- stories/components/cart/cart-upsell-menu.js
+++ stories/components/cart/cart-upsell-menu.js
@@ -13,6 +13,7 @@ export function Overview() {
 		template: `
 			<cart-upsell-menu
 				:upsell="upsell"
+				:wallet="wallet"
 				@add-product="handleAddProduct"
 			/>
 		`,
@@ -36,6 +37,9 @@ export function Overview() {
 						name: 'Cleanse 3',
 						imageUrl: 'https://cdn.shopify.com/s/files/1/0252/3833/9670/products/Combo-thedailystart_1440x1440_97ce3c8e-8fc9-404b-9c7a-9a3e852babfb.png?v=1594826551',
 					}],
+				},
+				wallet: {
+					tier: 'INSIDER',
 				},
 			};
 		},
