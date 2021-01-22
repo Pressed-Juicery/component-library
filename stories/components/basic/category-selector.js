f5311e22bb49fa67d20262c480b7216a44234949
vinnihoke <vinni.hoke@gmail.com>
2021-01-22 10:55:41 -0800

    feat(category-selector): adding selected and categoryId to story

--- stories/components/basic/category-selector.js
+++ stories/components/basic/category-selector.js
@@ -8,14 +8,20 @@ export default {
 export function Overview() {
 	return {
 		components: { CategorySelector },
-		template: '<category-selector :category="category" />',
+		template: '<category-selector :category="category" @selected="handleSelected" />',
 		data() {
 			return {
 				category: {
 					iconUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/icons/categories/shots-bottom-aligned.svg',
 					name: 'Shots',
+					categoryId: '/something/somethingelse',
 				},
 			};
+		},
+		methods: {
+			handleSelected(selectedCategoryId) {
+				console.log(selectedCategoryId);
+			},
 		},
 	};
 }
