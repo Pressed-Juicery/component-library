a47228348a68ae5e851490827e26eb13225b663d
vinnihoke <vinni.hoke@gmail.com>
2021-01-22 13:50:43 -0800

    feat(current-offers): cleaning up tabs and adding extra promotion with long title

--- stories/components/basic/current-promotions.js
+++ stories/components/basic/current-promotions.js
@@ -13,19 +13,24 @@ export function Overview() {
 			return {
 				currentPromotions: [
 					{
-					  handle: '6-for-29',
-					  name: 'ANY 6 ITEMS FOR $29',
-					  helpText: 'Applied at checkout',
+						handle: '6-for-29',
+						name: 'ANY 6 ITEMS FOR $29',
+						helpText: 'Applied at checkout',
 					},
 					{
-					  handle: '4-for-20',
-					  name: 'ANY 4 ITEMS & 1 SHOT FOR $20',
-					  helpText: 'Applied at cart',
+						handle: '4-for-20',
+						name: 'ANY 4 ITEMS & 1 SHOT FOR $20',
+						helpText: 'Applied at cart',
 					},
 					{
-					  handle: '10-for-60',
-					  name: 'ANY 10 ITEMS & 1 SHOT FOR $0',
-					  helpText: 'Applied never',
+						handle: '10-for-60',
+						name: 'ANY 10 ITEMS & 1 SHOT FOR $0',
+						helpText: 'Applied never',
+					},
+					{
+						handle: '10-for-60',
+						name: 'EXTRA EXTRA EXTRA EXTRA EXTRA EXTRA EXTRA EXTRA EXTRA LONG BOY',
+						helpText: 'Applied never',
 					},
 				],
 			};
