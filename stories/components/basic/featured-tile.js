89a475972bc87b0fb23506556e22be669b677bc3
vinnihoke <vinni.hoke@gmail.com>
2021-01-22 10:53:05 -0800

    feat(featured-tile): adding selected and categoryId to the story

--- stories/components/basic/featured-tile.js
+++ stories/components/basic/featured-tile.js
@@ -8,15 +8,21 @@ export default {
 export function Overview() {
 	return {
 		components: { FeaturedTile },
-		template: '<featured-tile :tile="tile" />',
+		template: '<featured-tile :tile="tile" @selected="handleSelected" />',
 		data() {
 			return {
 				tile: {
 					imageUrl: 'https://d3e4ixgvd0ibii.cloudfront.net/shopify/pages/home/home__cleanse-tile.jpg',
 					title: 'Test Tile',
 					description: 'This is a description of the test tile.',
+					categoryId: 'something/somethingelse',
 				},
 			};
+		},
+		methods: {
+			handleSelected(selectedCategoryId) {
+				console.log(selectedCategoryId);
+			},
 		},
 	};
 }
