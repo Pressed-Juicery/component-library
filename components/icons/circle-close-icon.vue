0005fedd501aea08a53d2b63142b5428268462b7
vinnihoke <vinni.hoke@gmail.com>
2021-03-11 09:17:26 -0800

    feat(shop-page-components): add circle close icon

---
+++ components/icons/circle-close-icon.vue
@@ -0,0 +1,14 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<g fill="none" fill-rule="evenodd">
+			<g>
+				<g transform="translate(-366 -681) translate(366 681)">
+					<circle cx="12" cy="12" r="11.25" stroke="#282321" stroke-width="1.5"/>
+					<g stroke="#292321" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+						<path d="M.998 9.557L9.439.557M.998.557L9.44 9.44" transform="translate(7 7)"/>
+					</g>
+				</g>
+			</g>
+		</g>
+	</svg>
+</template>
